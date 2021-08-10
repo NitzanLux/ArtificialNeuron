b53dde9130c3aaea45de8a87aa279ccdabddb4c3
goolux <30436343+goolux@users.noreply.github.com>
2021-08-10 14:46:12 +0300

    Add files via upload

--- neuronal_model.py
+++ neuronal_model.py
@@ -1,12 +1,7 @@
 import os
-import pickle
 from typing import Tuple
-
-import torch
+from synapse_tree import SectionNode, SectionType
 import torch.nn as nn
-from torch.nn.utils import weight_norm
-
-from synapse_tree import SectionNode, SectionType, NUMBER_OF_PREVIUSE_SEGMENTS_IN_BRANCH
 
 # set SEED
 os.environ["SEED"] = "42"
@@ -17,22 +12,21 @@ SYNAPSE_DIMENTION = 3
 epsp_num = 60
 ipsp_num = 20
 
+from subprocess import call
 import os
 import numpy as np
 
 FNULL = open(os.devnull, 'w')
-if torch.cuda.is_available():
-    coda = "cuda"
-    torch.cuda.set_device('cuda:0')
-    print("\n******   Cuda available!!!   *****")
-else:
-    dev = "cpu"
+
 # set SEED
 os.environ["SEED"] = "42"
 
 # ======================
 #     TCN Components
 # ======================
+import torch
+import torch.nn as nn
+from torch.nn.utils import weight_norm
 
 
 class SegmentNetwork(nn.Module):
@@ -74,17 +68,15 @@ class BranchLeafBlock(SegmentNetwork):
 
         self.batch_normalization = torch.nn.BatchNorm2d(channels_number)
 
-        padding_factor = self.keep_dimensions_by_padding_claculator((input_shape[0], 1),
-                                                                    (kernel_size_1d, input_shape[1]), stride,
-                                                                    dilation)
+        padding_factor = self.keep_dimensions_by_padding_claculator(input_shape, (kernel_size_1d, 1), stride, dilation)
 
         self.conv1d = weight_norm(nn.Conv2d(channels_number, channel_output, (kernel_size_1d, input_shape[1]),
                                             stride=stride, padding=padding_factor,
-                                            dilation=dilation)) # todo: weight_norm???
+                                            dilation=dilation))  # todo: weight_norm???
         # todo: collapse?
         self.init_weights()
         self.net = nn.Sequential(self.conv2d, self.activation_function,
-                                 self.batch_normalization, self.conv1d, self.activation_function)
+                                 self.batch_normalization, self.conv1d, self.activation_function, )
 
     def init_weights(self):
         self.conv2d.weight.data.normal_(0, 0.01)
@@ -101,16 +93,20 @@ class BranchBlock(SegmentNetwork):
                  dilation=1,
                  activation_function=nn.ReLU):
         super(BranchBlock, self).__init__()
-        padding_factor = self.keep_dimensions_by_padding_claculator(
-            (input_shape[0], input_shape[1] - NUMBER_OF_PREVIUSE_SEGMENTS_IN_BRANCH), kernel_size_2d, stride, dilation)
-        self.conv2d_x = weight_norm(nn.Conv2d(channel_input, channel_output, kernel_size_2d,  # todo: weight_norm???
+        padding_factor = self.keep_dimensions_by_padding_claculator(input_shape, kernel_size_2d, stride, dilation)
+
+        self.conv2d_prev = weight_norm(
+            nn.Conv2d(channels_number, channels_number, kernel_size_2d,  # todo: weight_norm???
+                      stride=stride, padding=padding_factor, dilation=dilation))
+        self.conv2d_x = weight_norm(nn.Conv2d(channel_input, channels_number, kernel_size_2d,  # todo: weight_norm???
                                               stride=stride, padding=padding_factor, dilation=dilation))
         self.activation_function = activation_function()
 
-        self.batch_normalization = torch.nn.BatchNorm2d(channel_output)
-        padding_factor = self.keep_dimensions_by_padding_claculator((input_shape[0], 1), (kernel_size_1d, 1), stride,
-                                                                    dilation)
-        self.conv1d = weight_norm(nn.Conv2d(channel_output, channel_output, (kernel_size_1d, input_shape[1]),
+        padding_factor = self.keep_dimensions_by_padding_claculator(input_shape, (kernel_size_1d, 1), stride, dilation)
+
+        self.batch_normalization = torch.nn.BatchNorm2d(channels_number)
+
+        self.conv1d = weight_norm(nn.Conv2d(channels_number, channel_output, (kernel_size_1d, 1),
                                             stride=stride, padding=padding_factor,
                                             dilation=dilation))
         self.init_weights()
@@ -119,12 +115,12 @@ class BranchBlock(SegmentNetwork):
     def init_weights(self):
         self.conv1d.weight.data.normal_(0, 0.01)
         self.conv2d_x.weight.data.normal_(0, 0.01)
-        # self.conv2d_prev.weight.data.normal_(0, 0.01)
+        self.conv2d_prev.weight.data.normal_(0, 0.01)
 
     def forward(self, prev_segment, x):
         x = self.conv2d_x(x)
         # x = self.conv2d_x(x.unsqueeze(2)).squeeze(2)
-        # prev_segment = self.conv2d_prev(prev_segment.unsqueeze(2).squeeze(2))
+        prev_segment = self.conv2d_prev(prev_segment.unsqueeze(2).squeeze(2))
         out = torch.cat((x, prev_segment), dim=SYNAPSE_DIMENTION)
         out = self.net(out)
         return out
@@ -137,17 +133,14 @@ class IntersectionBlock(SegmentNetwork):
                  activation_function=nn.ReLU):
         super(IntersectionBlock, self).__init__()
         padding_factor = self.keep_dimensions_by_padding_claculator(input_shape, (kernel_size_1d, 1),
-
                                                                     stride, dilation)
 
-        self.conv1d_1 = weight_norm(nn.Conv2d(channel_output, channels_number, (kernel_size_1d, 1),
+        self.conv1d_1 = weight_norm(nn.Conv2d(channels_number, channel_output, (kernel_size_1d, 1),
                                               stride=stride, padding=padding_factor,
                                               dilation=dilation))
         self.activation = activation_function()
         self.batch_normalization = torch.nn.BatchNorm2d(channels_number)
-        padding_factor = self.keep_dimensions_by_padding_claculator((input_shape[0], 1), (kernel_size_1d, 1),
-                                                                    stride, dilation)
-        self.conv1d_2 = weight_norm(nn.Conv2d(channels_number, channel_output, (kernel_size_1d, input_shape[1]),
+        self.conv1d_2 = weight_norm(nn.Conv2d(channels_number, channel_output, (kernel_size_1d, 1),
                                               stride=stride, padding=padding_factor,
                                               dilation=dilation))
         self.net = nn.Sequential(self.conv1d_1, self.activation, self.batch_normalization, self.conv1d_2)
@@ -159,18 +152,17 @@ class IntersectionBlock(SegmentNetwork):
 
 
 class RootBlock(SegmentNetwork):
-    def __init__(self, input_shape: Tuple[int, int], channels_number):
+    def __init__(self, input_shape: Tuple[int, int],channels_number):
         super(RootBlock, self).__init__()
-
-        self.spike_prediction = nn.Conv2d(channels_number, 1, kernel_size=(1, input_shape[1]))
-        self.voltage_prediction = nn.Conv2d(channels_number, 1, kernel_size=(1, input_shape[1]))
+        self.spike_prediction = nn.Conv2d(channels_number, 1, (1,input_shape[1]))
+        self.voltage_prediction = nn.Conv2d(channels_number, 1, (1,input_shape[1]))
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         v = self.voltage_prediction(x)
-        s = self.spike_prediction(x)
-        s = self.sigmoid(s)
-        return s, v
+        x = self.spike_prediction(x)
+        x = self.sigmoid(x)
+        return v, x
 
 
 class NeuronConvNet(nn.Module):
@@ -178,24 +170,23 @@ class NeuronConvNet(nn.Module):
                  dilation, channel_input, channels_number, channel_output,
                  activation_function=nn.ReLU, include_dendritic_voltage_tracing=True):
         super(NeuronConvNet, self).__init__()
-        assert kernel_size_1d%2==1 and kernel_size_2d%2==1 ,"cannot assert even kernel size"
         self.include_dendritic_voltage_tracing = include_dendritic_voltage_tracing
         self.segment_tree = segment_tree
         self.segemnt_ids = dict()
         self.modules_dict = nn.ModuleDict()
-        self.is_cuda=False
         input_shape = (0, 0)  # default for outer scope usage
         for segment in segment_tree:
             self.segemnt_ids[segment] = segment.id
             param_number = segment.get_number_of_parameters_for_nn()
             input_shape = (time_domain_shape, param_number)
+            print(segment.type)
             if segment.type == SectionType.BRANCH:
                 self.modules_dict[self.segemnt_ids[segment]] = BranchBlock(input_shape, channel_input, channels_number,
                                                                            channel_output, kernel_size_2d,
                                                                            kernel_size_1d, stride, dilation,
-                                                                           activation_function) # todo: add parameters
+                                                                           activation_function)  # todo: add parameters
             elif segment.type == SectionType.BRANCH_INTERSECTION:
-                self.modules_dict[self.segemnt_ids[segment]] = IntersectionBlock(input_shape, channel_input,
+                self.modules_dict[self.segemnt_ids[segment]] = IntersectionBlock(input_shape, channels_number,
                                                                                  channels_number,
                                                                                  channel_output, kernel_size_2d,
                                                                                  kernel_size_1d, stride, dilation,
@@ -205,50 +196,36 @@ class NeuronConvNet(nn.Module):
                                                                                channels_number,
                                                                                channel_output, kernel_size_2d,
                                                                                kernel_size_1d, stride, dilation,
-                                                                               activation_function) # todo: add parameters
+                                                                               activation_function)  # todo: add parameters
 
             elif segment.type == SectionType.SOMA:
-                self.last_layer = RootBlock(input_shape, channel_output)  # the last orgen in tree is the root
+                self.last_layer = RootBlock(input_shape,channel_output)  # the last orgen in tree is the root
             else:
                 assert False, "Type not found"
 
-
-    def cuda(self,**argv):
-        super(NeuronConvNet,self).cuda(**argv)
-        self.is_cuda=True
-
     def forward(self, x):
-        x = x.type(torch.cuda.DoubleTensor)if self.is_cuda else x.type(torch.DoubleTensor)
         if self.include_dendritic_voltage_tracing:  # todo add functionality
             pass
         representative_dict = {}
         out = None
         for node in self.segment_tree:
             if node.type == SectionType.BRANCH_LEAF:
-                input = x[..., list(node.synapse_nodes_dict.keys())]
-                representative_dict[node.representative] = self.modules_dict[self.segemnt_ids[node]](input)
+                representative_dict[node.representative] = self.modules_dict[self.segemnt_ids[node]](
+                    x[..., list(node.synapse_nodes_dict.keys())])
 
-                assert representative_dict[node.representative].shape[3] == 1
 
             elif node.type == SectionType.BRANCH_INTERSECTION:
-                indexs = [child.representative for child in node.prev_nodes]
-                input = [representative_dict[i] for i in indexs]
+                indexs = node.get_prev_node_representative()
+                input = [representative_dict[out] for out in indexs]
                 input = torch.cat(input, dim=SYNAPSE_DIMENTION)
                 representative_dict[node.representative] = self.modules_dict[self.segemnt_ids[node]](input)
-
-                assert representative_dict[node.representative].shape[3] == 1
 
             elif node.type == SectionType.BRANCH:
                 input = x[..., list(node.synapse_nodes_dict.keys())]
                 representative_dict[node.representative] = self.modules_dict[self.segemnt_ids[node]](
                     representative_dict[node.prev_nodes[0].representative], input)
-                assert representative_dict[node.representative].shape[3] == 1
-
             elif node.type == SectionType.SOMA:
-                indexs = [child.representative for child in node.prev_nodes]
-                input = [representative_dict[i] for i in indexs]
-                input = torch.cat(input, dim=SYNAPSE_DIMENTION)
-                out = self.last_layer(input)
+                out = self.last_layer(representative_dict[0])
                 break
             else:
                 assert False, "Type not found"
