c9f8339214b7af86e04e99a438af734846b95501
nlux <nlux@cs.huji.ac.il>
2021-09-22 22:44:29 +0300

    update weights lr

--- fit_CNN.py
+++ fit_CNN.py
@@ -143,26 +143,26 @@ def learning_parameters_iter() -> Generator[Tuple[int, int, float, Tuple[float, 
         DVT_loss_mult_factor = 0
     epoch_in_each_step = config.num_epochs // 5 + (config.num_epochs % 5 != 0)
     for i in range(epoch_in_each_step):
-        learning_rate_per_epoch = 1 / (np.sqrt(config.batch_counter+1) * 1000)
-        loss_weights_per_epoch = [1.0, 1/(np.sqrt(config.batch_counter + 1)), DVT_loss_mult_factor * 0.00005]
+        learning_rate_per_epoch = 1 / ((config.batch_counter+1) * 1000)
+        loss_weights_per_epoch = [1.0, 1/((config.batch_counter + 1)), DVT_loss_mult_factor * 0.00005]
         yield config.epoch_size, learning_rate_per_epoch, loss_weights_per_epoch, sigma / (config.batch_counter + 1)
     for i in range(epoch_in_each_step):
-        learning_rate_per_epoch = 1 / (np.sqrt(config.batch_counter+1) * 1000)
-        loss_weights_per_epoch = [1.0, 1/(np.sqrt(config.batch_counter + 1)), DVT_loss_mult_factor * 0.00003]
+        learning_rate_per_epoch = 1 / ((config.batch_counter+1) * 1000)
+        loss_weights_per_epoch = [1.0, 1/((config.batch_counter + 1)), DVT_loss_mult_factor * 0.00003]
         yield config.epoch_size, learning_rate_per_epoch, loss_weights_per_epoch, sigma / (config.batch_counter + 1)
     for i in range(epoch_in_each_step):
-        learning_rate_per_epoch = 1 / (np.sqrt(config.batch_counter+1) * 1000)
-        loss_weights_per_epoch = [1.0, 1/(np.sqrt(config.batch_counter + 1)), DVT_loss_mult_factor * 0.00001]
+        learning_rate_per_epoch = 1 / ((config.batch_counter+1) * 1000)
+        loss_weights_per_epoch = [1.0, 1/((config.batch_counter + 1)), DVT_loss_mult_factor * 0.00001]
         yield config.epoch_size, learning_rate_per_epoch, loss_weights_per_epoch, sigma / (config.batch_counter + 1)
 
     for i in range(config.num_epochs // 5):
-        learning_rate_per_epoch = 1 / (np.sqrt(config.batch_counter+1) * 1000)
-        loss_weights_per_epoch = [1.0, 1/(np.sqrt(config.batch_counter + 1)), DVT_loss_mult_factor * 0.0000001]
+        learning_rate_per_epoch = 1 / ((config.batch_counter+1) * 1000)
+        loss_weights_per_epoch = [1.0, 1/((config.batch_counter + 1)), DVT_loss_mult_factor * 0.0000001]
         yield config.epoch_size, learning_rate_per_epoch, loss_weights_per_epoch, sigma / (config.batch_counter + 1)
 
     for i in range(config.num_epochs // 5 + config.num_epochs % 5):
-        learning_rate_per_epoch = 1 / (np.sqrt(config.batch_counter+1) * 1000)
-        loss_weights_per_epoch = [1.0, 1/(np.sqrt(config.batch_counter + 1)), DVT_loss_mult_factor * 0.00000001]
+        learning_rate_per_epoch = 1 / ((config.batch_counter+1) * 1000)
+        loss_weights_per_epoch = [1.0, 1/((config.batch_counter + 1)), DVT_loss_mult_factor * 0.00000001]
         yield config.epoch_size, learning_rate_per_epoch, loss_weights_per_epoch, sigma / (config.batch_counter + 1)
 
 
