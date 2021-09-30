284e93a9d99cf1f6b7c63af0a5d66f8b6b6eed58
nlux <nlux@cs.huji.ac.il>
2021-09-30 13:58:27 +0300

    update cluster sender

--- cluster_sender.py
+++ cluster_sender.py
@@ -1,12 +1,5 @@
-import socket
-from typing import List
-from configuration_factory import config_factory, load_config_file
+from slurm_job import *
 
-
-class TaskSender():
-    def __init__(self, resend_number: int,configs:List[str]):
-        self.local_address = socket.gethostbyname(socket.gethostname())
-        self.resend_number = resend_number
 
 
 if __name__ == '__main__':
--- send_mail_test.py
+++
@@ -1,3 +0,0 @@
-from email_by_demand import send_mail
-
-send_mail("nitzan.luxembourg@mail.huji.ac.il","finished run","finished run send mail test")
