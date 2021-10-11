ce9ff067d8a12ae45fa3fcfd076c0f7ed229a161
nlux <nlux@cs.huji.ac.il>
2021-10-11 13:45:37 +0300

    evaluation debugging

--- evaluation.py
+++ evaluation.py
@@ -53,7 +53,7 @@ def plot_network_and_actual_results(file_path: [str, List[str]], model_path: [st
     first_path_name = r_match.group(0)
     for p in model_path:
         network = neuronal_model.NeuronConvNet.load(p)
-        network.cpu()
+        # network.cpu()
         regex_match = re.search('(?<=TCN__)[0-9]{4}-[0-9]{2}-[0-9]{2}__[0-9]{2}_[0-9]{2}__ID_[0-9]+(?=\.pkl)', p)
         model_id = regex_match.group(0)
         out = network(X_batch)
