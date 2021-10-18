60e1dbcecf131a64934999319a7db97cb1239c49
nlux <nlux@cs.huji.ac.il>
2021-10-18 21:39:56 +0300

    update display accuracy to torch

--- evaluation.py
+++ evaluation.py
@@ -56,12 +56,14 @@ def plot_network_and_actual_results(file_path: [str, List[str]], model_path: [st
     # except Exception as e:
     #     logging.error(model_path[0])
     #     raise e
+    first_path_name=""
     for p in model_path:
         network = neuronal_model.NeuronConvNet.load(p)
         # network.cpu()
         regex_match = re.search('(?:.*)(?<=TCN__)[0-9]{4}-[0-9]{2}-[0-9]{2}__[0-9]{2}_[0-9]{2}__ID_[0-9]+(?=\.pkl)?', p)
         try:
             model_id = regex_match.group(0)
+            first_path_name=model_id
         except Exception as e:
             logging.error(p)
             raise e
