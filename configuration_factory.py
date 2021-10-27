3d5535bca343a4de5bab40f6af1cab570d5a5e9e
nlux <nlux@cs.huji.ac.il>
2021-10-27 13:35:24 +0300

    upload code

--- configuration_factory.py
+++ configuration_factory.py
@@ -56,8 +56,8 @@ def config_factory(save_model_to_config_dir=True, config_new_path=None, generate
                    **kargs):
     ##default values can be overridden by kargs
     config = AttrDict(input_window_size=200, num_segments=2 * 639, num_syn_types=1,
-                      epoch_size=5, num_epochs=10, batch_size_train=5, batch_size_validation=2,
-                      train_file_load=0.5, valid_file_load=0.5,spike_probability= 0.5,
+                      num_epochs=15000, epoch_size=30, batch_size_train=15, batch_size_validation=5,
+                      train_file_load=0.5, valid_file_load=0.5,spike_probability= 0.3,
                       optimizer_type="AdamW", optimizer_params={},
                       batch_counter=0, epoch_counter=0,  # default counter
                       torch_seed=42, numpy_seed=21, random_seed=12,init_weights_sd=0.05,
@@ -71,12 +71,12 @@ def config_factory(save_model_to_config_dir=True, config_new_path=None, generate
                                  time_domain_shape=config.input_window_size,
                                  # kernel_size_2d=3,
                                  # kernel_size_1d=9,
-                                 kernel_size=9,
+                                 kernel_size=51,
                                  number_of_layers=3,
                                  stride=1,
                                  dilation=1,
                                  channel_input_number=1,  # synapse number
-                                 inner_scope_channel_number=7,
+                                 inner_scope_channel_number=9,
                                  channel_output_number=5,
                                  activation_function_name="LeakyReLU",
                                  activation_function_kargs=dict(negative_slope=0.25),
@@ -146,14 +146,14 @@ def generate_config_files_multiple_seeds(config_path: [str, Dict], number_of_con
 
 
 if __name__ == '__main__':
-    config_dynamic = config_factory(model_tag="simplest")
-    # configs_dynamic = generate_config_files_multiple_seeds(config_dynamic, 2)
-    # config_static = config_factory(dynamic_learning_params=False)
-    # configs_static = generate_config_files_multiple_seeds(config_static, 1)
-    # configs_to_read = configs_dynamic+[config_factory(loss_function="loss_zero_mse_on_spikes")]
-    #
-    # with open(os.path.join(MODELS_DIR,"model_for_evaluation_mask_mse.json"), 'w') as file:
-    #     file.write(json.dumps(configs_to_read)  )# use `json.loads` to do the reverse
+    config_dynamic = config_factory()
+    configs_dynamic = generate_config_files_multiple_seeds(config_dynamic, 2)
+    config_static = config_factory(dynamic_learning_params=False)
+    configs_static = generate_config_files_multiple_seeds(config_static, 1)
+    configs_to_read = configs_dynamic+[config_factory(loss_function="loss_zero_mse_on_spikes")]
+
+    with open(os.path.join(MODELS_DIR,"model_for_evaluation_mask_mse.json"), 'w') as file:
+        file.write(json.dumps(configs_to_read)  )# use `json.loads` to do the reverse
 
 
     # config = load_config_file("models/NMDA/simplest_model_dynamic_NMDA_Tree_TCN__2021-09-30__16_51__ID_78714/simplest_model_dynamic_NMDA_Tree_TCN__2021-09-30__16_51__ID_78714.config")
