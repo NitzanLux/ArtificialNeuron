4b39d46e9d0847b7a30d178274a9538c3ca23c0b
nlux <nlux@cs.huji.ac.il>
2021-11-02 22:09:14 +0200

    update slurm_job_execution.sh in a way that you need the parameters in the command line

--- fit_CNN.py
+++ fit_CNN.py
@@ -267,7 +267,7 @@ def run_fit_cnn():
     # send_mail("nitzan.luxembourg@mail.huji.ac.il","somthing went wrong",e)
     # raise e
 
-
-run_fit_cnn()
+if __name__ == "__main__":
+    run_fit_cnn()
 
 # send_mail("nitzan.luxembourg@mail.huji.ac.il","finished run","finished run")
