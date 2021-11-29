b772682998b25284e8d4bbb9da666ed8d3842c2f
goolux <30436343+goolux@users.noreply.github.com>
2021-11-29 13:35:57 +0200

    recursive function was fixed

--- neuron_network/node_network/recursive_neuronal_model.py
+++ neuron_network/node_network/recursive_neuronal_model.py
@@ -118,7 +118,6 @@ class RecursiveNeuronModel(nn.Module):
         BIOPHYSICAL_MODEL_TAMPLATE_PATH = r'../../NEURON_models_maker/L5PC_NEURON_simulation/L5PCtemplate_2.hoc'
         h.load_file('nrngui.hoc')
         h.load_file("import3d.hoc")
-        h.nrn_load_dll(DLL_FILE_PATH)
         h.load_file(BIOPHYSICAL_MODEL_PATH)
         h.load_file(BIOPHYSICAL_MODEL_TAMPLATE_PATH)
         L5PC = h.L5PCtemplate(MORPHOLOGY_PATH_L5PC)
