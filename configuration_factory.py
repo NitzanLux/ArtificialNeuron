2235d5dec0bafa4b61d6c5078584f11ead6519df
goolux <30436343+goolux@users.noreply.github.com>
2021-12-08 15:27:43 +0200

    update network behavior and configurations

--- configuration_factory.py
+++ configuration_factory.py
@@ -65,8 +65,8 @@ def config_factory(save_model_to_config_dir=True, config_new_path=None, generate
     config = AttrDict(config_version=1.0,input_window_size=300, num_segments=2 * 639, num_syn_types=1,
                       num_epochs=15000, epoch_size=30, batch_size_train=30, batch_size_validation=200,
                       train_file_load=0.5, valid_file_load=0.5, spike_probability=0.5,
-                      # files_filter_regex=".*exBas_0_1100_inhBasDiff_-1100_600__exApic_0_1100_inhApicDiff_-1100_600_SpTemp[^\\/\.]*\.p",
-                      files_filter_regex=".*",
+                      files_filter_regex=".*exBas_0_1100_inhBasDiff_-1100_600__exApic_0_1100_inhApicDiff_-1100_600_SpTemp[^\\/\.]*\.p",
+                      # files_filter_regex=".*",
                       optimizer_type="AdamW", optimizer_params={},
                       batch_counter=0, epoch_counter=0,  # default counter
                       torch_seed=42, numpy_seed=21, random_seed=12, init_weights_sd=0.05,
