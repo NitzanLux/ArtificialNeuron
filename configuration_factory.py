0ad3dd93052d24186e54dfd962ced5053ca249e1
goolux <30436343+goolux@users.noreply.github.com>
2021-12-27 14:16:08 +0200

    clip norm models

--- configuration_factory.py
+++ configuration_factory.py
@@ -181,7 +181,7 @@ def generate_config_files_multiple_seeds(config_path: [str, Dict], number_of_con
 
 if __name__ == '__main__':
     configs = []
-    for i in [0.04,0.1,0.5,1,2.5]:
+    for i in [0.04,0.1,0.5,1]:
         config_morpho_0 =config_factory(dynamic_learning_params=False, architecture_type="LAYERED_TEMPORAL_CONV",
                         model_tag="cm_%0.2f_model"%i,skip_conections=True,
                                  inter_module_skip_connections=True,batch_size_validation=200,spike_probability=0.5,clip_gradients_factor=i)
