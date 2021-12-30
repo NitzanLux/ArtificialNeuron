448d7b3c4d77774ec201c5baf4477c375ada94a6
goolux <30436343+goolux@users.noreply.github.com>
2021-12-30 18:07:09 +0200

    overfitting cheacking

--- configuration_factory.py
+++ configuration_factory.py
@@ -71,7 +71,7 @@ def config_factory(save_model_to_config_dir=True, config_new_path=None, generate
                       batch_counter=0, epoch_counter=0,  # default counter
                       torch_seed=42, numpy_seed=21, random_seed=12, init_weights_sd=0.05,
                       dynamic_learning_params=True,
-                      constant_loss_weights=[50., 1., 0., 0], constant_sigma=1.2, constant_learning_rate=0.001,
+                      constant_loss_weights=[50., 1., 0., 0], constant_sigma=1.2, constant_learning_rate=0.0001,
                       dynamic_learning_params_function="learning_parameters_iter_slow_50_with_constant_weights",
                       config_path="", model_tag="complex_constant_model", model_path=None,
                       loss_function="bcel_mse_dvt_loss")
@@ -182,11 +182,11 @@ def generate_config_files_multiple_seeds(config_path: [str, Dict], number_of_con
 if __name__ == '__main__':
     configs = []
     config_morpho_0 =config_factory(dynamic_learning_params=False, architecture_type="LAYERED_TEMPORAL_CONV",
-                    model_tag="cm_cheack_overfitting_8_model",skip_conections=True,
+                    model_tag="cm_cheack_overfitting_9_model",skip_conections=True,
                          inter_module_skip_connections=True,batch_size_validation=200,spike_probability=0.5,clip_gradients_factor=2)
     configs.append(config_morpho_0)
 
-    with open(os.path.join(MODELS_DIR, "cm_overcheack_model_8.json"), 'w') as file:
+    with open(os.path.join(MODELS_DIR, "cm_overcheack_model_9.json"), 'w') as file:
         file.write(json.dumps(configs))  # use `json.loads` to do the reverse
         # file.write(json.dumps([config_morpho_0]))  # use `json.loads` to do the reverse
 
