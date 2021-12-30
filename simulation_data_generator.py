be69e2d453091708614db4ef870ea721de20cc7c
goolux <30436343+goolux@users.noreply.github.com>
2021-12-30 18:08:33 +0200

    overfitting cheacking

--- simulation_data_generator.py
+++ simulation_data_generator.py
@@ -16,7 +16,7 @@ class SimulationDataGenerator():
     'Characterizes a dataset for PyTorch'
 
     def __init__(self, sim_experiment_files, buffer_size_in_files=12, epoch_size=None,
-                 batch_size=8, sample_ratio_to_shuffle=1, window_size_ms=300, file_load=0.3, DVT_PCA_model=None,
+                 batch_size=8, sample_ratio_to_shuffle=0.8, window_size_ms=300, file_load=0.3, DVT_PCA_model=None,
                  ignore_time_from_start=20, y_train_soma_bias=-67.7, y_soma_threshold=-55.0, y_DTV_threshold=3.0,
                  shuffle_files=False, include_DVT=False, is_shuffle_data=False, number_of_traces_from_file=None, number_of_files=None):
         'data generator initialization'
