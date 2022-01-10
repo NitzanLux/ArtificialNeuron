8a1c7e5bdb3a35ab31be0e997372c0c582d19e68
goolux <30436343+goolux@users.noreply.github.com>
2022-01-10 19:35:09 +0200

    senity cheack

--- configuration_factory.py
+++ configuration_factory.py
@@ -69,7 +69,7 @@ def config_factory(save_model_to_config_dir=True, config_new_path=None, generate
                       train_file_load=0.5, valid_file_load=0.5, spike_probability=0.5,
                       # files_filter_regex=".*exBas_0_1100_inhBasDiff_-1100_600__exApic_0_1100_inhApicDiff_-1100_600_SpTemp[^\\/\.]*\.p",
                       files_filter_regex=".*",
-                      optimizer_type="AdamW", optimizer_params={'eps':1e-6},
+                      optimizer_type="AdamW", optimizer_params={'eps':1e-8},
                       clip_gradients_factor=1.5, lr_decay_factor=0.75, lr_patience_factor=100,
                       batch_counter=0, epoch_counter=0,  # default counter
                       torch_seed=42, numpy_seed=21, random_seed=12, init_weights_sd=0.05,
@@ -85,19 +85,19 @@ def config_factory(save_model_to_config_dir=True, config_new_path=None, generate
                                  time_domain_shape=config.input_window_size,
                                  # kernel_size_2d=3,
                                  # kernel_size_1d=9,
-                                 number_of_layers_root= 3, number_of_layers_leaf=2, number_of_layers_intersection=5,
-                                 number_of_layers_branch_intersection=3,
+                                 number_of_layers_root= 4, number_of_layers_leaf=4, number_of_layers_intersection=5,
+                                 number_of_layers_branch_intersection=4,
                                  david_layers = [55,13,13,13,13,13,13],
                                  skip_connections=True,
                                  inter_module_skip_connections=True,
-                                 kernel_size=41,
+                                 kernel_size=25,
                                  # number_of_layers=2,
                                  stride=1,
                                  padding=0,
                                  dilation=1,
                                  channel_input_number=1278,  # synapse number
-                                 inner_scope_channel_number=25,
-                                 channel_output_number=25,
+                                 inner_scope_channel_number=29,
+                                 channel_output_number=29,
                                  activation_function_name="LeakyReLU",
                                  activation_function_kargs=dict(negative_slope=0.5),
                                  include_dendritic_voltage_tracing=False)
@@ -188,13 +188,13 @@ if __name__ == '__main__':
     config_morpho_0 =config_factory(#loss_function='loss_zero_mse_on_spikes',
                                     dynamic_learning_params=False#,optimizer_type='RMSprop'
                                     ,dynamic_learning_params_function="learning_parameters_iter_with_constant_weights", architecture_type="LAYERED_TEMPORAL_CONV",
-                    model_tag="threshold_had_been_changed",skip_conections=True,
+                    model_tag="heavy",skip_conections=True,
                                      accumulate_loss_batch_factor=1,
-                         inter_module_skip_connections=False,batch_size_validation=200,clip_gradients_factor=5,constant_learning_rate=0.005)
+                         inter_module_skip_connections=False,batch_size_validation=200,clip_gradients_factor=1,constant_learning_rate=0.005)
 
     # configs.append(config_morpho_0)
     configs = generate_config_files_multiple_seeds(config_morpho_0,2)
-    with open(os.path.join(MODELS_DIR, "causal_conv_l9.json"), 'w') as file:
+    with open(os.path.join(MODELS_DIR, "causal_conv_l10.json"), 'w') as file:
         file.write(json.dumps(configs))  # use `json.loads` to do the reverse
         # file.write(json.dumps([config_morpho_0]))  # use `json.loads` to do the reverse
 
