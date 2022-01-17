207886935e96ffd941705f39053bdc4431415f08
goolux <30436343+goolux@users.noreply.github.com>
2022-01-17 15:54:58 +0200

    update mixed precision

--- configuration_factory.py
+++ configuration_factory.py
@@ -199,8 +199,14 @@ if __name__ == '__main__':
                     model_tag="heavy",optimizer_type='RMSprop',
                                      accumulate_loss_batch_factor=4,spike_probability=None,prediction_length=1,
                         batch_size_validation=200,batch_size_train=10,clip_gradients_factor=5,constant_learning_rate=0.005)
-
+    config_morpho_1 =config_factory(#loss_function='focalbcel_mse_loss',
+                                    dynamic_learning_params=False#,optimizer_type='RMSprop'
+                                    ,dynamic_learning_params_function="learning_parameters_iter_with_constant_weights", architecture_type="LAYERED_TEMPORAL_CONV",
+                    model_tag="heavy",optimizer_type='AdamW',
+                                     accumulate_loss_batch_factor=4,spike_probability=None,prediction_length=1,
+                        batch_size_validation=200,batch_size_train=10,clip_gradients_factor=5,constant_learning_rate=0.005)
     configs.append(config_morpho_0)
+    configs.append(config_morpho_1)
     # configs = generate_config_files_multiple_seeds(config_morpho_0,2)
     with open(os.path.join(MODELS_DIR, "sliding_window_d3.json"), 'w') as file:
         file.write(json.dumps(configs))  # use `json.loads` to do the reverse
