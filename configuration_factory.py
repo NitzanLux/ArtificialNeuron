1518d860b3b2e4226824e7a68f50c6d31956dac9
goolux <30436343+goolux@users.noreply.github.com>
2022-01-17 16:29:40 +0200

    update mixed precision

--- configuration_factory.py
+++ configuration_factory.py
@@ -58,7 +58,7 @@ def surround_with_default_config_values(**kargs):
                       # files_filter_regex=".*exBas_0_1100_inhBasDiff_-1100_600__exApic_0_1100_inhApicDiff_-1100_600_SpTemp[^\\/\.]*\.p",
                       files_filter_regex=".*",
                       optimizer_type="AdamW",optimizer_params={},# optimizer_params={'eps':1e-8},
-                      clip_gradients_factor=1.5, lr_decay_factor=0.75, lr_patience_factor=30,
+                      clip_gradients_factor=1.5, lr_decay_factor=0.75, lr_patience_factor=10,
                       batch_counter=0, epoch_counter=0,  # default counter
                       torch_seed=42, numpy_seed=21, random_seed=12, init_weights_sd=0.05,
                       dynamic_learning_params=True,
@@ -199,16 +199,16 @@ if __name__ == '__main__':
                     model_tag="heavy",optimizer_type='RMSprop',
                                      accumulate_loss_batch_factor=4,spike_probability=None,prediction_length=1,
                         batch_size_validation=200,batch_size_train=10,clip_gradients_factor=5,constant_learning_rate=0.005)
-    config_morpho_1 =config_factory(#loss_function='focalbcel_mse_loss',
-                                    dynamic_learning_params=False#,optimizer_type='RMSprop'
-                                    ,dynamic_learning_params_function="learning_parameters_iter_with_constant_weights", architecture_type="LAYERED_TEMPORAL_CONV",
-                    model_tag="heavy",optimizer_type='AdamW',
-                                     accumulate_loss_batch_factor=4,spike_probability=None,prediction_length=1,
-                        batch_size_validation=200,batch_size_train=10,clip_gradients_factor=5,constant_learning_rate=0.005)
+    # config_morpho_1 =config_factory(#loss_function='focalbcel_mse_loss',
+    #                                 dynamic_learning_params=False#,optimizer_type='RMSprop'
+    #                                 ,dynamic_learning_params_function="learning_parameters_iter_with_constant_weights", architecture_type="LAYERED_TEMPORAL_CONV",
+    #                 model_tag="heavy",optimizer_type='AdamW',
+    #                                  accumulate_loss_batch_factor=4,spike_probability=None,prediction_length=1,
+    #                     batch_size_validation=200,batch_size_train=10,clip_gradients_factor=5,constant_learning_rate=0.005)
     configs.append(config_morpho_0)
-    configs.append(config_morpho_1)
+    # configs.append(config_morpho_1)
     # configs = generate_config_files_multiple_seeds(config_morpho_0,2)
-    with open(os.path.join(MODELS_DIR, "sliding_window_d3.json"), 'w') as file:
+    with open(os.path.join(MODELS_DIR, "sliding_window_d4.json"), 'w') as file:
         file.write(json.dumps(configs))  # use `json.loads` to do the reverse
         # file.write(json.dumps([config_morpho_0]))  # use `json.loads` to do the reverse
 
