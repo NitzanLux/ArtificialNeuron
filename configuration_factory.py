75009f3b4e92db5a59f9a60ea122905898ecf65b
goolux <30436343+goolux@users.noreply.github.com>
2022-01-19 13:00:28 +0200

    update mixed precision

--- configuration_factory.py
+++ configuration_factory.py
@@ -194,12 +194,12 @@ def generate_config_files_multiple_seeds(config_path: [str, Dict], number_of_con
 if __name__ == '__main__':
     configs = []
     # for i in [1,2.5,5,10]:
-    config_morpho_0 =config_factory(loss_function='focalbcel_mse_loss',
-                                    dynamic_learning_params=False#,optimizer_type='RMSprop'
-                                    ,dynamic_learning_params_function="learning_parameters_iter_with_constant_weights", architecture_type="LAYERED_TEMPORAL_CONV",
-                    model_tag="heavy",optimizer_type='AdamW',
-                                     accumulate_loss_batch_factor=4,spike_probability=None,prediction_length=2000,
-                        batch_size_validation=200,batch_size_train=5,clip_gradients_factor=2.5,constant_learning_rate=0.005)
+    # config_morpho_0 =config_factory(loss_function='focalbcel_mse_loss',
+    #                                 dynamic_learning_params=False#,optimizer_type='RMSprop'
+    #                                 ,dynamic_learning_params_function="learning_parameters_iter_with_constant_weights", architecture_type="LAYERED_TEMPORAL_CONV",
+    #                 model_tag="heavy",optimizer_type='AdamW',
+    #                                  accumulate_loss_batch_factor=4,spike_probability=None,prediction_length=2000,
+    #                     batch_size_validation=200,batch_size_train=5,clip_gradients_factor=2.5,constant_learning_rate=0.005)
     # config_morpho_1 =config_factory(#loss_function='focalbcel_mse_loss',
     #                                 dynamic_learning_params=False#,optimizer_type='RMSprop'
     #                                 ,dynamic_learning_params_function="learning_parameters_iter_with_constant_weights", architecture_type="LAYERED_TEMPORAL_CONV",
@@ -208,8 +208,18 @@ if __name__ == '__main__':
     #                     batch_size_validation=200,batch_size_train=10,clip_gradients_factor=5,constant_learning_rate=0.005)
     # configs.append(config_morpho_0)
     # configs.append(config_morpho_1)
-    configs = generate_config_files_multiple_seeds(config_morpho_0,2)
-    with open(os.path.join(MODELS_DIR, "silver_grass_mash.json"), 'w') as file:
+    for i in ['AdamW','RMSprop']:
+        config_morpho_0 = config_factory(loss_function='focalbcel_mse_loss',
+                                         dynamic_learning_params=False  # ,optimizer_type='RMSprop'
+                                         ,
+                                         dynamic_learning_params_function="learning_parameters_iter_with_constant_weights",
+                                         architecture_type="LAYERED_TEMPORAL_CONV",
+                                         model_tag="heavy", optimizer_type=i,
+                                         accumulate_loss_batch_factor=4, spike_probability=None, prediction_length=2000,
+                                         batch_size_validation=200, batch_size_train=5, clip_gradients_factor=2.5,
+                                         constant_learning_rate=0.005)
+        configs.extend(generate_config_files_multiple_seeds(config_morpho_0,2))
+    with open(os.path.join(MODELS_DIR, "robust_microwave.json"), 'w') as file:
         file.write(json.dumps(configs))  # use `json.loads` to do the reverse
         # file.write(json.dumps([config_morpho_0]))  # use `json.loads` to do the reverse
 
