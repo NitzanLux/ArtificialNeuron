600b9e7dab9ee64737b2b4bab6054ac9e7d1e66f
goolux <30436343+goolux@users.noreply.github.com>
2022-01-24 20:37:45 +0200

    added general evaluation and update bugs in simulation_

--- configuration_factory.py
+++ configuration_factory.py
@@ -223,7 +223,7 @@ if __name__ == '__main__':
                                          dynamic_learning_params_function="learning_parameters_iter_with_constant_weights",
                                          architecture_type="LAYERED_TEMPORAL_CONV",
                                          model_tag="heavy", optimizer_type=i,
-                                         accumulate_loss_batch_factor=1, spike_probability=None, prediction_length=1000,
+                                         accumulate_loss_batch_factor=2, spike_probability=None, prediction_length=1000,
                                          batch_size_validation=200, batch_size_train=5, clip_gradients_factor=2.5,
                                          constant_learning_rate=0.005)
         configs.extend(generate_config_files_multiple_seeds(config_morpho_0,2))
