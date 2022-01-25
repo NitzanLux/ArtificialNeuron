0ea45cfdf1113acb909ac58bc23aca665ef65f3d
goolux <30436343+goolux@users.noreply.github.com>
2022-01-25 17:57:14 +0200

    added general evaluation and update bugs in simulation_

--- configuration_factory.py
+++ configuration_factory.py
@@ -222,13 +222,13 @@ if __name__ == '__main__':
                                          ,
                                          dynamic_learning_params_function="learning_parameters_iter_with_constant_weights",
                                          architecture_type="LAYERED_TEMPORAL_CONV",
-                                         model_tag="heavy", optimizer_type=i,
+                                         model_tag="heavy_%s"%i, optimizer_type=i,
                                          accumulate_loss_batch_factor=1, spike_probability=None, prediction_length=1000,
                                          batch_size_validation=200, batch_size_train=5, clip_gradients_factor=2.5,
                                          constant_learning_rate=0.005)
-        configs.append(config_morpho_0)
-        # configs.extend(generate_config_files_multiple_seeds(config_morpho_0,2))
-    with open(os.path.join(MODELS_DIR, "testing_evaluation_1.json"), 'w') as file:
+        # configs.append(config_morpho_0)
+        configs.extend(generate_config_files_multiple_seeds(config_morpho_0,2))
+    with open(os.path.join(MODELS_DIR, "evaluation_multiple_optimizers.json"), 'w') as file:
         file.write(json.dumps(configs))  # use `json.loads` to do the reverse
         # file.write(json.dumps([config_morpho_0]))  # use `json.loads` to do the reverse
 
