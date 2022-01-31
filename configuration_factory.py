b01ef9c5357469fcad84cede1a2b330fea59ffa0
goolux <30436343+goolux@users.noreply.github.com>
2022-01-31 13:21:12 +0200

    MSE only configuration

--- configuration_factory.py
+++ configuration_factory.py
@@ -69,12 +69,12 @@ def surround_with_default_config_values(**kargs):
                                  time_domain_shape=config.input_window_size,
                                  # kernel_size_2d=3,
                                  # kernel_size_1d=9,
-                                 number_of_layers_root=7, number_of_layers_leaf=7, number_of_layers_intersection=7,
+                                 number_of_layers_root=5, number_of_layers_leaf=5, number_of_layers_intersection=7,
                                  number_of_layers_branch_intersection=7,
                                  david_layers = [55,13,13,13,13,13,13],
                                  skip_connections=True,
                                  inter_module_skip_connections=True,
-                                 kernel_size=31,
+                                 kernel_size=21,
                                  # number_of_layers=2,
                                  stride=1,
                                  padding=0,
@@ -223,9 +223,9 @@ if __name__ == '__main__':
                                          dynamic_learning_params_function="learning_parameters_iter_with_constant_weights",
                                          architecture_type="LAYERED_TEMPORAL_CONV",
                                          model_tag="%s%s"%(i,configurations_name), optimizer_type=i,
-                                         accumulate_loss_batch_factor=2, spike_probability=None, prediction_length=1000,
+                                         accumulate_loss_batch_factor=5, spike_probability=None, prediction_length=1000,
 
-                                         batch_size_validation=200, batch_size_train=5, clip_gradients_factor=10.,
+                                         batch_size_validation=200, batch_size_train=2, clip_gradients_factor=10.,
                                          constant_learning_rate=0.005)
         # configs.append(config_morpho_0)
         configs.extend(generate_config_files_multiple_seeds(config_morpho_0,2))
