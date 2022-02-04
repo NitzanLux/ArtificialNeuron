d4a3890ad5fb7ee45f05d4cee65a4a1df5c025f8
goolux <30436343+goolux@users.noreply.github.com>
2022-02-04 19:23:13 +0200

    updated accumulate loss

--- configuration_factory.py
+++ configuration_factory.py
@@ -16,7 +16,9 @@ from synapse_tree import SectionNode
 synapse_type = 'NMDA'
 include_DVT = False
 num_DVT_components = 20 if synapse_type == 'NMDA' else 30
-CURRENT_VERSION=1.3
+CURRENT_VERSION = 1.3
+
+
 def generate_model_name(additional_str: str = ''):
     model_ID = np.random.randint(100000)
     modelID_str = 'ID_%d' % (model_ID)
@@ -44,21 +46,22 @@ def save_config(config, path: [str, None] = None):
     return config
 
 
-
-
 def surround_with_default_config_values(**kargs):
     ##default values can be overridden by kargs
-    config = AttrDict(config_version=CURRENT_VERSION, input_window_size=200, prediction_length=1, num_segments=2 * 639, num_syn_types=1,
-                      num_epochs=15000, epoch_size=5, batch_size_train=5, accumulate_loss_batch_factor=4, batch_size_validation=200,
+    config = AttrDict(config_version=CURRENT_VERSION, input_window_size=200, prediction_length=1, num_segments=2 * 639,
+                      num_syn_types=1,
+                      num_epochs=15000, epoch_size=5, batch_size_train=5, accumulate_loss_batch_factor=4,
+                      batch_size_validation=200,
                       train_file_load=0.5, valid_file_load=0.5, spike_probability=0.5,
                       # files_filter_regex=".*exBas_0_1100_inhBasDiff_-1100_600__exApic_0_1100_inhApicDiff_-1100_600_SpTemp[^\\/\.]*\.p",
-                      files_filter_regex=".*",freeze_node_factor=None,
-                      optimizer_type="AdamW",optimizer_params={},# optimizer_params={'eps':1e-8},
-                      clip_gradients_factor=1.5, lr_decay_factor=0.75, lr_patience_factor=200,scheduler_cooldown_factor=200,
+                      files_filter_regex=".*", freeze_node_factor=None,
+                      optimizer_type="AdamW", optimizer_params={},  # optimizer_params={'eps':1e-8},
+                      clip_gradients_factor=1.5, lr_decay_factor=0.75, lr_patience_factor=400,
+                      scheduler_cooldown_factor=200,
                       batch_counter=0, epoch_counter=0,  # default counter
                       torch_seed=42, numpy_seed=21, random_seed=12, init_weights_sd=0.05,
                       dynamic_learning_params=True,
-                      constant_loss_weights=[100., 1., 100., 0], constant_sigma=1.2, constant_learning_rate=0.001,
+                      constant_loss_weights=[100., 1., 0., 0], constant_sigma=1.2, constant_learning_rate=0.001,
                       dynamic_learning_params_function="learning_parameters_iter_per_batch",
                       config_path="", model_tag="complex_constant_model", model_path=None,
                       loss_function="bcel_mse_dvt_loss")
@@ -71,7 +74,7 @@ def surround_with_default_config_values(**kargs):
                                  # kernel_size_1d=9,
                                  number_of_layers_root=6, number_of_layers_leaf=7, number_of_layers_intersection=7,
                                  number_of_layers_branch_intersection=7,
-                                 david_layers = [55,13,13,13,13,13,13],
+                                 david_layers=[55, 13, 13, 13, 13, 13, 13],
                                  skip_connections=True,
                                  inter_module_skip_connections=True,
                                  kernel_size=31,
@@ -93,14 +96,15 @@ def surround_with_default_config_values(**kargs):
 
 
 def load_config_file(path: str) -> AttrDict:
-    if path[-len('.config'):]!='.config':
-        path+='.config'
+    if path[-len('.config'):] != '.config':
+        path += '.config'
     with open(path, 'r') as file:
         config = json.load(file)
-    config=AttrDict(config)
+    config = AttrDict(config)
     if config.config_version < CURRENT_VERSION:
         config = surround_with_default_config_values(**config)
     return config
+
 
 # ------------------------------------------------------------------
 # define network architecture params
@@ -134,7 +138,7 @@ def config_factory(save_model_to_config_dir=True, config_new_path=None, generate
                 model = davids_network.DavidsNeuronNetwork(config)
             elif config.network_architecture_structure == "recursive":
                 L5PC = get_L5PC()
-                model = recursive_neuronal_model.RecursiveNeuronModel.build_david_data_model(config,L5PC)
+                model = recursive_neuronal_model.RecursiveNeuronModel.build_david_data_model(config, L5PC)
             else:
                 model = neuronal_model.NeuronConvNet.build_model_from_config(config)
             config.model_path = config_new_path + [config.model_filename]
@@ -142,21 +146,19 @@ def config_factory(save_model_to_config_dir=True, config_new_path=None, generate
         else:
             if config.architecture_type == "DavidsNeuronNetwork":
                 model = davids_network.DavidsNeuronNetwork.load(config)
-            elif  config.network_architecture_structure=="recursive":
+            elif config.network_architecture_structure == "recursive":
                 L5PC = get_L5PC()
-                model =recursive_neuronal_model.RecursiveNeuronModel.build_david_data_model(config,L5PC)
+                model = recursive_neuronal_model.RecursiveNeuronModel.build_david_data_model(config, L5PC)
                 # model.load(config)
             else:
                 model = neuronal_model.NeuronConvNet.load(os.path.join(MODELS_DIR, *config.model_path))
 
             config.model_path = config_new_path + [config.model_filename]
             model.save(os.path.join(MODELS_DIR, *config.model_path))
-        print(model.count_parameters() ,config.model_filename)
+        print(model.count_parameters(), config.model_filename)
     config.config_path = config_new_path + ['%s.config' % config.model_filename]
     save_config(config)
     return config.config_path
-
-
 
 
 def overwrite_config(config, **kargs):
@@ -215,21 +217,20 @@ if __name__ == '__main__':
     #                                      batch_size_validation=200, batch_size_train=5, clip_gradients_factor=2.5,
     #                                      constant_learning_rate=0.005)
     #     configs.extend(generate_config_files_multiple_seeds(config_morpho_0,2))
-    configurations_name="mea"
-    for i in ['AdamW','NAdam']:#,'RMSprop']:
+    configurations_name = "overshot_1"
+    for i in ['AdamW', 'NAdam']:  # ,'RMSprop']:
         config_morpho_0 = config_factory(loss_function='focalbcel_mse_mae_loss',
                                          dynamic_learning_params=False  # ,optimizer_type='RMSprop'
                                          ,
                                          dynamic_learning_params_function="learning_parameters_iter_with_constant_weights",
                                          architecture_type="LAYERED_TEMPORAL_CONV",
-                                         model_tag="%s%s"%(i,configurations_name), optimizer_type=i,
+                                         model_tag="%s%s" % (i, configurations_name), optimizer_type=i,
                                          accumulate_loss_batch_factor=5, spike_probability=None, prediction_length=1000,
 
                                          batch_size_validation=200, batch_size_train=5, clip_gradients_factor=5.,
                                          constant_learning_rate=0.005)
         # configs.append(config_morpho_0)
-        configs.extend(generate_config_files_multiple_seeds(config_morpho_0,2))
-    with open(os.path.join(MODELS_DIR, "%s.json"%configurations_name), 'w') as file:
+        configs.extend(generate_config_files_multiple_seeds(config_morpho_0, 2))
+    with open(os.path.join(MODELS_DIR, "%s.json" % configurations_name), 'w') as file:
         file.write(json.dumps(configs))  # use `json.loads` to do the reverse
         # file.write(json.dumps([config_morpho_0]))  # use `json.loads` to do the reverse
-
