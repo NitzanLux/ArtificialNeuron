94ec657932894456569498eef3853c5b0aec14f5
goolux <30436343+goolux@users.noreply.github.com>
2022-02-06 13:52:32 +0200

    updated accumulate loss

--- configuration_factory.py
+++ configuration_factory.py
@@ -56,7 +56,7 @@ def surround_with_default_config_values(**kargs):
                       # files_filter_regex=".*exBas_0_1100_inhBasDiff_-1100_600__exApic_0_1100_inhApicDiff_-1100_600_SpTemp[^\\/\.]*\.p",
                       files_filter_regex=".*", freeze_node_factor=None,
                       optimizer_type="AdamW", optimizer_params={},  # optimizer_params={'eps':1e-8},
-                      clip_gradients_factor=1.5, lr_decay_factor=0.5, lr_patience_factor=250,
+                      clip_gradients_factor=1.5, lr_decay_factor=0.5, lr_patience_factor=200,
                       scheduler_cooldown_factor=200,
                       batch_counter=0, epoch_counter=0,  # default counter
                       torch_seed=42, numpy_seed=21, random_seed=12, init_weights_sd=0.05,
@@ -72,19 +72,19 @@ def surround_with_default_config_values(**kargs):
                                  time_domain_shape=config.input_window_size,
                                  # kernel_size_2d=3,
                                  # kernel_size_1d=9,
-                                 number_of_layers_root=3, number_of_layers_leaf=3, number_of_layers_intersection=3,
-                                 number_of_layers_branch_intersection=3,
+                                 number_of_layers_root=2, number_of_layers_leaf=2, number_of_layers_intersection=2,
+                                 number_of_layers_branch_intersection=2,
                                  david_layers=[55, 13, 13, 13, 13, 13, 13],
                                  skip_connections=True,
                                  inter_module_skip_connections=True,
-                                 kernel_size=41,
+                                 kernel_size=71,
                                  # number_of_layers=2,
                                  stride=1,
                                  padding=0,
                                  dilation=1,
                                  channel_input_number=1278,  # synapse number
-                                 inner_scope_channel_number=41,
-                                 channel_output_number=41,
+                                 inner_scope_channel_number=61,
+                                 channel_output_number=61,
                                  activation_function_name="LeakyReLU",
                                  activation_function_kargs=dict(negative_slope=0.25),
                                  include_dendritic_voltage_tracing=False)
@@ -217,7 +217,7 @@ if __name__ == '__main__':
     #                                      batch_size_validation=200, batch_size_train=5, clip_gradients_factor=2.5,
     #                                      constant_learning_rate=0.005)
     #     configs.extend(generate_config_files_multiple_seeds(config_morpho_0,2))
-    configurations_name = "overshot_2"
+    configurations_name = "short_and_wide"
     for i in ['AdamW', 'NAdam']:  # ,'RMSprop']:
         config_morpho_0 = config_factory(loss_function='focalbcel_mse_mae_loss',
                                          dynamic_learning_params=False  # ,optimizer_type='RMSprop'
