0855ea4b88d3b132e8edf5a57f80049089f41f31
goolux <30436343+goolux@users.noreply.github.com>
2022-02-09 11:12:50 +0200

    updated accumulate loss

--- general_evaluation.py
+++ general_evaluation.py
@@ -308,7 +308,7 @@ args = parser.parse_args()
 print(args)
 with open(os.path.join(MODELS_DIR, "%s.json" % args.configs_path), 'r') as file:
     configs = json.load(file)
-for i, conf in enumerate(conf):
+for i, conf in enumerate(configs):
     ModelEvaluator.build_and_save(conf)
 
 
