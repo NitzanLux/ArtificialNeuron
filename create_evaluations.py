8f8b8fdf93e4387a7098d4f702aafa7ea8a92c5f
goolux <30436343+goolux@users.noreply.github.com>
2022-02-09 11:06:36 +0200

    updated accumulate loss

--- create_evaluations.py
+++ create_evaluations.py
@@ -11,10 +11,7 @@ parser.add_argument(dest="config_path_or_json", type=str,
                     help='configuration file for path')
 args = parser.parse_args()
 print(args)
-config = configuration_factory.load_config_file(args.config_path)
-
-
 job_factory = SlurmJobFactory("cluster_logs")
-job_factory.send_job("%%s_evaluation" % (configs_file),
+job_factory.send_job("%%s_evaluation" % (args.config_path_or_json),
                      'python3 $(dirname "$path")/general_evaluation.py %s $SLURM_JOB_ID' % str(
                          os.path.join(MODELS_DIR, configs_file)), True)
