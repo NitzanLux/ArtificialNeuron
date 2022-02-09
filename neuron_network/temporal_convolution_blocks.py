a0d959650e62589fa0a7ff7233ff35ddd9d35b61
goolux <30436343+goolux@users.noreply.github.com>
2022-02-09 17:03:18 +0200

    updated innerscope to be fit s.t the output channels will be the bottleneck

--- neuron_network/temporal_convolution_blocks.py
+++ neuron_network/temporal_convolution_blocks.py
@@ -43,11 +43,12 @@ class Base1DConvolutionBlock(nn.Module):
         self.skip_connections = skip_connections
         if inner_scope_channel_number is None:
             self.inner_scope_channel_number=input_shape[0]
-            print("hhhh",flush=True)
         else:
-            print("iner scope",flush=True)
             self.inner_scope_channel_number = inner_scope_channel_number
-        self.channel_output_number = channel_output_number
+        if channel_output_number is None:
+            self.channel_output_number=input_shape[0]
+        else:
+            self.channel_output_number = channel_output_number
         self.channel_input_number = input_shape[0]
         # self.batch_norm = nn.BatchNorm1d(input_shape[0])#todo debugging
 
