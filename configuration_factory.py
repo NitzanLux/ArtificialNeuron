e7cbeab0d8883334be10ef5885978f37934dbec6
goolux <30436343+goolux@users.noreply.github.com>
2022-02-11 18:52:35 +0200

    fixed bottle neck add None in case we want to keep the inter block channel number the same.

--- configuration_factory.py
+++ configuration_factory.py
@@ -56,7 +56,7 @@ def surround_with_default_config_values(**kargs):
                       # files_filter_regex=".*exBas_0_1100_inhBasDiff_-1100_600__exApic_0_1100_inhApicDiff_-1100_600_SpTemp[^\\/\.]*\.p",
                       files_filter_regex=".*", freeze_node_factor=None,
                       optimizer_type="AdamW", optimizer_params={},  # optimizer_params={'eps':1e-8},
-                      lr_scheduler='CyclicLR',lr_scheduler_params=dict(max_lr=constant_learning_rate,step_size_up=1000,base_lr=0.00003),
+                      lr_scheduler='CyclicLR',lr_scheduler_params=dict(max_lr=0.001,step_size_up=1000,base_lr=0.00003),
                       # lr_scheduler_params=dict(clip_gradients_factor=1.5, lr_decay_factor=0.5, lr_patience_factor=75)
                       scheduler_cooldown_factor=150,
                       batch_counter=0, epoch_counter=0,  # default counter
