7109ba30cdfb0a800e25204aca22200dfdc982e5
goolux <30436343+goolux@users.noreply.github.com>
2022-02-17 11:25:40 +0200

    fixed bottle neck add None in case we want to keep the inter block channel number the same.

--- configuration_factory.py
+++ configuration_factory.py
@@ -246,7 +246,7 @@ if __name__ == '__main__':
     #                                      batch_size_validation=200, batch_size_train=5, clip_gradients_factor=2.5,
     #                                      constant_learning_rate=0.005)
     #     configs.extend(generate_config_files_multiple_seeds(config_morpho_0,2))
-    configurations_name = "glu_test_two"
+    configurations_name = "glu_2"
     for i in ['NAdam' ,'RMSprop','Adamax']:
         config_morpho_0 = config_factory(loss_function='focalbcel_mse_mae_loss',
                                          dynamic_learning_params=False  # ,optimizer_type='RMSprop'
