8a2b05a3025780440621eaf2db507ffd11a1d51a
goolux <30436343+goolux@users.noreply.github.com>
2022-02-20 10:35:11 +0200

    fixed bottle neck add None in case we want to keep the inter block channel number the same.

---
+++ evaluate_models_cluster_script.py
@@ -0,0 +1,16 @@
+import argparse
+import os
+import json
+from model_evaluation import ModelEvaluator
+
+parser = argparse.ArgumentParser(description='Add configuration file')
+parser.add_argument(dest="configs_path", type=str,
+                    help='configuration file for path')
+parser.add_argument(dest="job_id", help="the job id", type=str)
+args = parser.parse_args()
+print(args)
+with open(os.path.join(MODELS_DIR, "%s.json" % args.configs_path), 'r') as file:
+    configs = json.load(file)
+for i, conf in enumerate(configs):
+    ModelEvaluator.build_and_save(os.path.join(MODELS_DIR, *conf))
+
--- model_evaluation.py
+++ model_evaluation.py
@@ -18,7 +18,6 @@ from general_aid_function import *
 from neuron_network import neuronal_model
 import plotly.express as px
 import datetime
-import argparse
 import json
 from general_variables import *
 
@@ -303,17 +302,6 @@ class ModelEvaluator():
         evaluation_engine.save()
         end_time=datetime.datetime.now()
         print("evaluation took %0.1f minutes"%((end_time-start_time).total_seconds()/60.))
-
-# parser = argparse.ArgumentParser(description='Add configuration file')
-# parser.add_argument(dest="configs_path", type=str,
-#                     help='configuration file for path')
-# parser.add_argument(dest="job_id", help="the job id", type=str)
-# args = parser.parse_args()
-# print(args)
-# with open(os.path.join(MODELS_DIR, "%s.json" % args.configs_path), 'r') as file:
-#     configs = json.load(file)
-# for i, conf in enumerate(configs):
-#     ModelEvaluator.build_and_save(os.path.join(MODELS_DIR, *conf))
 
 
 
