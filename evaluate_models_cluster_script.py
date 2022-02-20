64b81ae1df20702fb4de2b82b9bba442db9a2299
goolux <30436343+goolux@users.noreply.github.com>
2022-02-20 10:38:25 +0200

    fixed bottle neck add None in case we want to keep the inter block channel number the same.

--- evaluate_models_cluster_script.py
+++ evaluate_models_cluster_script.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import json
 from model_evaluation import ModelEvaluator
+from project_path import *
 
 parser = argparse.ArgumentParser(description='Add configuration file')
 parser.add_argument(dest="configs_path", type=str,
