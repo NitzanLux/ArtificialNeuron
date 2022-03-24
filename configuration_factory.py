b55b9eb28ea2494352e4a383ec7a066d270e3966
goolux <30436343+goolux@users.noreply.github.com>
2022-03-24 11:33:00 +0200

    fixed bottle neck add None in case we want to keep the inter block channel number the same.

--- configuration_factory.py
+++ configuration_factory.py
@@ -110,7 +110,7 @@ def load_config_file(path: str) -> AttrDict:
     with open(path, 'r') as file:
         config = json.loads(file.read())
     config = AttrDict(config)
-    config.constant_loss_weights=[10000., 1., 0., 0]
+    # config.constant_loss_weights=[10000., 1., 0., 0]
     # config.accumulate_loss_batch_factor=8
     # config.constant_learning_rate=0.0003
     # config.lr_scheduler='CyclicLR'
