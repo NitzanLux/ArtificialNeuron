8242ed0059f8e0f22eedaff8552f851fc4a12af8
goolux <30436343+goolux@users.noreply.github.com>
2022-05-07 15:53:49 +0300

    updated focal loss

--- configuration_factory.py
+++ configuration_factory.py
@@ -113,14 +113,6 @@ def load_config_file(path: str) -> AttrDict:
     with open(path, 'r') as file:
         config = json.loads(file.read())
     config = AttrDict(config)
-    # config.constant_loss_weights=[10000., 1., 0., 0]
-    # config.accumulate_loss_batch_factor=3
-    # config.clip_gradients_factor=2
-    # accumulate_loss_batch_factor = 2
-    # config.constant_learning_rate=0.0003
-    # config.optimizer_params['lr']=config.constant_learning_rate
-    # config.lr_scheduler=None
-    # config.lr_scheduler_params=dict(factor=0.5,cooldown=40, threshold=1e-5,patience =100,eps=7e-5)
     if config.config_version < CURRENT_VERSION :
         config = surround_with_default_config_values(**config)
     return config
@@ -267,7 +259,7 @@ if __name__ == '__main__':
                                          dynamic_learning_params_function="learning_parameters_iter_with_constant_weights",
                                          model_tag="%s_%s" % (configurations_name,i), optimizer_type=i,
                                          accumulate_loss_batch_factor=8, spike_probability=None, prediction_length=5800,
-                                         batch_size_validation=200, batch_size_train=8, clip_gradients_factor=1,
+                                         batch_size_validation=128, batch_size_train=8, clip_gradients_factor=1,
                                          constant_learning_rate=0.0007)
         # configs.append(config_morpho_0)
         configs.extend(generate_config_files_multiple_seeds(config_morpho_0, 2))
