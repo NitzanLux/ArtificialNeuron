33e530b6b2f70facfb54c637c4802d650b791988
goolux <30436343+goolux@users.noreply.github.com>
2022-05-09 11:29:47 +0300

    updated focal loss

--- configuration_factory.py
+++ configuration_factory.py
@@ -94,8 +94,8 @@ def surround_with_default_config_values(**kargs):
                                  padding=0,
                                  dilation=1,
                                  channel_input_number=1278,  # synapse number
-                                 inner_scope_channel_number=None,
-                                 channel_output_number=32,
+                                 inner_scope_channel_number=32,
+                                 channel_output_number=8,
                                  activation_function_name="LeakyReLU",
                                  # activation_function_kargs=dict(),
                                  activation_function_kargs=dict(negative_slope=0.01),
@@ -252,15 +252,15 @@ if __name__ == '__main__':
     #                                      batch_size_validation=200, batch_size_train=5, clip_gradients_factor=2.5,
     #                                      constant_learning_rate=0.005)
     #     configs.extend(generate_config_files_multiple_seeds(config_morpho_0,2))
-    configurations_name = "inter_4"
+    configurations_name = "inter_4_1"
     for i in ['AdamW']:
         config_morpho_0 = config_factory(loss_function='weighted_mse_loss_derivative',
                                          dynamic_learning_params=False  # ,optimizer_type='RMSprop'
                                          ,include_spikes=False,
                                          dynamic_learning_params_function="learning_parameters_iter_with_constant_weights",
                                          model_tag="%s_%s" % (configurations_name,i), optimizer_type=i,
-                                         accumulate_loss_batch_factor=16, spike_probability=None, prediction_length=5780,
-                                         batch_size_validation=64, batch_size_train=2, clip_gradients_factor=1,
+                                         accumulate_loss_batch_factor=3, spike_probability=None, prediction_length=5780,
+                                         batch_size_validation=64, batch_size_train=8, clip_gradients_factor=10,
                                          constant_learning_rate=0.001)
         # configs.append(config_morpho_0)
         configs.extend(generate_config_files_multiple_seeds(config_morpho_0, 2))
