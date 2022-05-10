12a7492dc718221a1ac3267672450ef39fe12b7d
goolux <30436343+goolux@users.noreply.github.com>
2022-05-10 12:03:58 +0300

    updated focal loss

--- configuration_factory.py
+++ configuration_factory.py
@@ -260,7 +260,7 @@ if __name__ == '__main__':
                                          dynamic_learning_params_function="learning_parameters_iter_with_constant_weights",
                                          model_tag="%s_%s" % (configurations_name,i), optimizer_type=i,
                                          accumulate_loss_batch_factor=2, spike_probability=None, prediction_length=5780,
-                                         batch_size_validation=64, batch_size_train=1, clip_gradients_factor=None,
+                                         batch_size_validation=64, batch_size_train=4, clip_gradients_factor=100,
                                          constant_learning_rate=0.001)
         # configs.append(config_morpho_0)
         configs.extend(generate_config_files_multiple_seeds(config_morpho_0, 2))
