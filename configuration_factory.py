e4a14e6549e54eb49468835046a2a975903a5edf
goolux <30436343+goolux@users.noreply.github.com>
2022-05-16 21:14:41 +0300

    updated focal loss

--- configuration_factory.py
+++ configuration_factory.py
@@ -77,7 +77,7 @@ def surround_with_default_config_values(**kargs):
                                  time_domain_shape=config.input_window_size,
                                  # kernel_size_2d=3,
                                  # kernel_size_1d=9,
-                                 number_of_layers_root=6, number_of_layers_leaf=10, number_of_layers_intersection=5,
+                                 number_of_layers_root=5, number_of_layers_leaf=10, number_of_layers_intersection=5,
                                  number_of_layers_branch_intersection=5,
                                  # david_layers=[55, 13, 13, 13, 13, 13, 13],
                                  glu_number_of_layers=0,
