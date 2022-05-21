3e8fc0d9c031a518261ceba63a8d1af50e478ef6
goolux <30436343+goolux@users.noreply.github.com>
2022-05-21 22:10:59 +0300

    updated focal loss

--- configuration_factory.py
+++ configuration_factory.py
@@ -95,7 +95,7 @@ def surround_with_default_config_values(**kargs):
                                  dilation=1,
                                  channel_input_number=1278,  # synapse number
                                  inner_scope_channel_number=32,
-                                 channel_output_number=1,
+                                 channel_output_number=4,
                                  activation_function_name="LeakyReLU",
                                  # activation_function_kargs=dict(),
                                  activation_function_kargs=dict(negative_slope=0.001),
