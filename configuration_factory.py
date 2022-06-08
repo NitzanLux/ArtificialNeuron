caf21fc681870b6085be05a6209c011855eefc01
goolux <30436343+goolux@users.noreply.github.com>
2022-06-08 15:29:58 +0300

    updated focal loss

--- configuration_factory.py
+++ configuration_factory.py
@@ -252,7 +252,7 @@ if __name__ == '__main__':
     #                                      batch_size_validation=200, batch_size_train=5, clip_gradients_factor=2.5,
     #                                      constant_learning_rate=0.005)
     #     configs.extend(generate_config_files_multiple_seeds(config_morpho_0,2))
-    configurations_name = "in_w_freeze"
+    configurations_name = "in_w_bcel"
     for i in ['AdamW']:
         config_morpho_0 = config_factory(loss_function='focalbcel_mse_loss',
                                          dynamic_learning_params=False  # ,optimizer_type='RMSprop'
