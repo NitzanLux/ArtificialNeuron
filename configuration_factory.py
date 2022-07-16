8e36cd816511b50c3f3426d3192ba6c50739e054
goolux <30436343+goolux@users.noreply.github.com>
2022-07-16 19:09:24 +0300

    examine the spatio temporal connections

--- configuration_factory.py
+++ configuration_factory.py
@@ -118,7 +118,7 @@ def load_config_file(path: str) -> AttrDict:
     config = json.loads(file_s)
     config = AttrDict(config)
     # config.include_spikes=True
-    config.batch_size_train=16
+    config.batch_size_train=8
     config.accumulate_loss_batch_factor=1
     config.prediction_length = (6000 - 600) // 4
     # lr_scheduler_params = dict(factor=0.1, cooldown=100, threshold=1e-5, patience=750, eps=5e-9)
