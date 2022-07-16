f71cdd7956aff9347db35c3b384c7a6b2cc1b057
goolux <30436343+goolux@users.noreply.github.com>
2022-07-16 17:56:14 +0300

    examine the spatio temporal connections

--- configuration_factory.py
+++ configuration_factory.py
@@ -79,7 +79,7 @@ def surround_with_default_config_values(**kargs):
                                  time_domain_shape=config.input_window_size,
                                  # kernel_size_2d=3,
                                  # kernel_size_1d=9,
-                                 kernel_sizes=[33]*7,number_of_layers_temp=0,number_of_layers_space=7,channel_number=[256]+[52]*7,
+                                 kernel_sizes=[33]*7,number_of_layers_temp=7,number_of_layers_space=7,channel_number=[256]+[52]*7,
                                  number_of_layers_root=7, number_of_layers_leaf=10, number_of_layers_intersection=5,
                                  number_of_layers_branch_intersection=5,
                                  # david_layers=[55, 13, 13, 13, 13, 13, 13],
@@ -235,19 +235,19 @@ def restore_last_n_configs(n=10):
 
 
 if __name__ == '__main__':
-    restore_last_n_configs(100)
-    # configs = []
-    # configurations_name = "davids"
-    # for i in ['AdamW']:
-    #     config_morpho_0 = config_factory(loss_function='focalbcel_mse_loss',
-    #                                      dynamic_learning_params=False,architecture_type='FullNeuronNetwork',
-    #                                      model_tag="%s_%s" % (configurations_name,i), optimizer_type=i,
-    #                                      accumulate_loss_batch_factor=1, spike_probability=None, prediction_length=(6000-600)//2,
-    #                                      batch_size_validation=32, batch_size_train=16,
-    #                                      constant_learning_rate=0.001)
-    #     # configs.append(config_morpho_0)
-    #     configs.extend(generate_config_files_multiple_seeds(config_morpho_0, 2))
-    # with open(os.path.join(MODELS_DIR, "%s.json" % configurations_name), 'w') as file:
-    #     file.write(json.dumps(configs))  # use `json.loads` to do the reverse
-    #     # file.write(json.dumps([config_morpho_0]))  # use `json.loads` to do the reverse
+    # restore_last_n_configs(100)
+    configs = []
+    configurations_name = "davids"
+    for i in ['AdamW']:
+        config_morpho_0 = config_factory(loss_function='focalbcel_mse_loss',
+                                         dynamic_learning_params=False,architecture_type='FullNeuronNetwork',
+                                         model_tag="%s_%s" % (configurations_name,i), optimizer_type=i,
+                                         accumulate_loss_batch_factor=1, spike_probability=None, prediction_length=(6000-600)//2,
+                                         batch_size_validation=32, batch_size_train=16,
+                                         constant_learning_rate=0.001)
+        # configs.append(config_morpho_0)
+        configs.extend(generate_config_files_multiple_seeds(config_morpho_0, 2))
+    with open(os.path.join(MODELS_DIR, "%s.json" % configurations_name), 'w') as file:
+        file.write(json.dumps(configs))  # use `json.loads` to do the reverse
+        # file.write(json.dumps([config_morpho_0]))  # use `json.loads` to do the reverse
     #
