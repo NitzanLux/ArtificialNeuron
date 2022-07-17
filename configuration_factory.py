19346ffb9367d0d9400103fe072231451fa8bdf4
goolux <30436343+goolux@users.noreply.github.com>
2022-07-17 11:16:43 +0300

    update tempspace and davids network params

--- configuration_factory.py
+++ configuration_factory.py
@@ -122,9 +122,9 @@ def load_config_file(path: str) -> AttrDict:
     # config.accumulate_loss_batch_factor=4
     # config.prediction_length = (6000 - 600) // 4
     # lr_scheduler_params = dict(factor=0.1, cooldown=100, threshold=1e-5, patience=750, eps=5e-9)
-    config.lr_scheduler_params=dict()
-    config.lr_scheduler=None
-    config.constant_learning_rate=0.0007
+    # config.lr_scheduler_params=dict()
+    # config.lr_scheduler=None
+    # config.constant_learning_rate=0.0007
     if config.config_version < CURRENT_VERSION :
         config = surround_with_default_config_values(**config)
     return config
