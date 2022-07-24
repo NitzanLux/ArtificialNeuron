48008473df4c38b3cfd2cf8dbf0316e21b183687
goolux <30436343+goolux@users.noreply.github.com>
2022-07-24 10:26:34 +0300

    updated causal convolution.

--- configuration_factory.py
+++ configuration_factory.py
@@ -79,7 +79,7 @@ def surround_with_default_config_values(**kargs):
                                  time_domain_shape=config.input_window_size,
                                  # kernel_size_2d=3,
                                  # kernel_size_1d=9,
-                                 kernel_sizes=[53]+[11]*6,number_of_layers_temp=7,number_of_layers_space=7,channel_number=[128]*7,space_kernel_sizes=[5]*7,
+                                 kernel_sizes=[53]+[11]*5,number_of_layers_temp=6,number_of_layers_space=7,channel_number=[128]*7,space_kernel_sizes=[5]*7,
                                  number_of_layers_root=5, number_of_layers_leaf=7, number_of_layers_intersection=5,
                                  number_of_layers_branch_intersection=5,
                                  # david_layers=[55, 13, 13, 13, 13, 13, 13],
