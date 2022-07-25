e3b47864f6a48ee919fec15aa853479b2a643439
goolux <30436343+goolux@users.noreply.github.com>
2022-07-25 14:53:23 +0300

    updated causal convolution.

--- configuration_factory.py
+++ configuration_factory.py
@@ -254,8 +254,8 @@ if __name__ == '__main__':
                                          architecture_type='FullNeuronNetwork',
                                          # architecture_type='LAYERED_TEMPORAL_CONV_N',
                                          model_tag="%s_%s" % (configurations_name,i), optimizer_type=i,clip_gradients_factor=None,
-                                         accumulate_loss_batch_factor=4, spike_probability=None, prediction_length=(6000-600)//2,
-                                         batch_size_validation=128, batch_size_train=32,
+                                         accumulate_loss_batch_factor=2, spike_probability=None, prediction_length=(6000-600)//2,
+                                         batch_size_validation=64, batch_size_train=64,
                                          constant_learning_rate=0.01)
         # configs.append(config_morpho_0)
         configs.extend(generate_config_files_multiple_seeds(config_morpho_0, 2))
