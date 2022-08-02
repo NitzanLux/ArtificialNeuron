707f838be5c4c705ff5d7dbdf5a3edece570ca8a
goolux <30436343+goolux@users.noreply.github.com>
2022-08-02 20:23:14 +0300

    reduction loader

--- reduction_simulation_execution.py
+++ reduction_simulation_execution.py
@@ -18,10 +18,10 @@ parser.add_argument(dest="directory", type=str,
 args = parser.parse_args()
 print(args)
 
-# number_of_cpus = args.number_of_cpus
+number_of_cpus = args.number_of_cpus
 directory = args.directory
 onlyfiles = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
-number_of_cpus = len(onlyfiles)
+# number_of_cpus = len(onlyfiles)
 
 job_factory = SlurmJobFactory("cluster_logs")
 files_per_cpu = ceil(len(onlyfiles)/number_of_cpus)
@@ -31,15 +31,24 @@ directory_name=os.path.dirname(directory)
 directory_name=os.path.basename(directory_name)
 
 params_string=''
+# for i,f in enumerate(onlyfiles):
+#     if i%number_of_cpus==0:
+#         params_string = " '" + str(os.path.join(directory, f)) + "' -d '" + directory_name + "_reduction'"
+#     else:
+#         params_string = " '" + str(os.path.join(directory, f)) + "'" + params_string
+#     if i%files_per_cpu==files_per_cpu-1 or i==len(onlyfiles)-1:
+#
+#         job_factory.send_job("%s_%s"%("reduction_simulation",directory_name),
+#                          'python3 $(dirname "$path")/simulate_L5PC_reduction_and_create_dataset.py %s -i $SLURM_JOB_ID' % ("-f"+params_string),filename_index=i//files_per_cpu)
+
 for i,f in enumerate(onlyfiles):
     if i%number_of_cpus==0:
-        params_string = " '" + str(os.path.join(directory, f)) + "' -d '" + directory_name + "_reduction'"
+        params_string = 'python3 $(dirname "$path")/simulate_L5PC_reduction_and_create_dataset.py %s -i $SLURM_JOB_ID'%("-f '" + str(os.path.join(directory, f)) + "' -d '" + directory_name + "_reduction'")
     else:
-        params_string = " '" + str(os.path.join(directory, f)) + "'" + params_string
+        params_string = params_string+'&& python3 $(dirname "$path")/simulate_L5PC_reduction_and_create_dataset.py %s -i $SLURM_JOB_ID'%("-f '" + str(os.path.join(directory, f)) + "' -d '" + directory_name + "_reduction'")
+
     if i%files_per_cpu==files_per_cpu-1 or i==len(onlyfiles)-1:
 
-        job_factory.send_job("%s_%s"%("reduction_simulation",directory_name),
-                         'python3 $(dirname "$path")/simulate_L5PC_reduction_and_create_dataset.py %s -i $SLURM_JOB_ID' % ("-f"+params_string),filename_index=i//files_per_cpu)
-
+        job_factory.send_job("%s_%s"%("reduction_simulation",directory_name), params_string,filename_index=i//files_per_cpu)
 
 
