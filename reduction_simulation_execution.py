657aa7cf8c4acc4c00d7b562e671b7934329c957
goolux <30436343+goolux@users.noreply.github.com>
2022-08-02 11:20:37 +0300

    update dataloader

--- reduction_simulation_execution.py
+++ reduction_simulation_execution.py
@@ -27,6 +27,7 @@ files_per_cpu = ceil(len(onlyfiles)/number_of_cpus)
 
 
 directory_name=os.path.basename(directory)
+print(directory_name)
 # with open(os.path.join(MODELS_DIR, "%s.json" % configs_file), 'r') as file:
 #     configs = json.load(file)
 
@@ -37,9 +38,9 @@ for i,f in enumerate(onlyfiles):
         params_string = "-f '" + str(os.path.join(directory, f)) + "' -d '" + directory_name + "_reduction'"
     else:
         params_string = "-f '" + str(os.path.join(directory, f)) + "' " + params_string
-    if i%files_per_cpu==files_per_cpu-1 or i==len(onlyfiles)-1:
-        job_factory.send_job("%s_%s"%("reduction_simulation",directory_name),
-                         'python3 $(dirname "$path")/simulate_L5PC_reduction_and_create_dataset.py %s -i $SLURM_JOB_ID' % (params_string),filename_index=i//files_per_cpu)
+    # if i%files_per_cpu==files_per_cpu-1 or i==len(onlyfiles)-1:
+        # job_factory.send_job("%s_%s"%("reduction_simulation",directory_name),
+        #                  'python3 $(dirname "$path")/simulate_L5PC_reduction_and_create_dataset.py %s -i $SLURM_JOB_ID' % (params_string),filename_index=i//files_per_cpu)
 
 
 
