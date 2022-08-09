f3551ece171b21b1a056f91a240e4336563bfb67
goolux <30436343+goolux@users.noreply.github.com>
2022-08-09 17:32:59 +0300

    add dropout to fc tcn

--- simulate_L5PC_ergodic_reduction.py
+++ simulate_L5PC_ergodic_reduction.py
@@ -82,21 +82,21 @@ def GetDistanceBetweenSections(sourceSection, destSection):
 # NMDA synapse
 def DefineSynapse_NMDA(segment, gMax=0.0004, NMDA_to_AMPA_g_ratio=1.0):
     return None
-    synapse = h.ProbAMPANMDA_David(segment)
-
-    synapse.tau_r_AMPA = 0.3
-    synapse.tau_d_AMPA = 3.0
-    synapse.tau_r_NMDA = 2.0
-    synapse.tau_d_NMDA = 70.0
-    synapse.gmax_AMPA = gMax
-    synapse.gmax_NMDA = gMax * NMDA_to_AMPA_g_ratio
-    synapse.e = 0
-    synapse.Use = 1
-    synapse.u0 = 0
-    synapse.Dep = 0
-    synapse.Fac = 0
-
-    return synapse
+    # synapse = h.ProbAMPANMDA_David(segment)
+    #
+    # synapse.tau_r_AMPA = 0.3
+    # synapse.tau_d_AMPA = 3.0
+    # synapse.tau_r_NMDA = 2.0
+    # synapse.tau_d_NMDA = 70.0
+    # synapse.gmax_AMPA = gMax
+    # synapse.gmax_NMDA = gMax * NMDA_to_AMPA_g_ratio
+    # synapse.e = 0
+    # synapse.Use = 1
+    # synapse.u0 = 0
+    # synapse.Dep = 0
+    # synapse.Fac = 0
+    #
+    # return synapse
 
 
 # GABA A synapse
