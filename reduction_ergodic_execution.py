28e216dd5b1e5be57bed574c4838b2f0e6c5657e
goolux <30436343+goolux@users.noreply.github.com>
2022-08-09 20:09:10 +0300

    add dropout to fc tcn

--- reduction_ergodic_execution.py
+++ reduction_ergodic_execution.py
@@ -47,9 +47,9 @@ for i,f in enumerate(onlyfiles):
     print(base_directory[:10]+directory_name)
 
     if i%files_per_cpu==0:
-        params_string = 'python3 $(dirname "$path")/simulate_L5PC_ergodic_reduction.py %s -i $SLURM_JOB_ID'%("-f '" + str(os.path.join(directory, f)) + "' -d '" + base_directory[:10]+directory_name + "_reduction'")
+        params_string = 'python3 $(dirname "$path")/simulate_L5PC_ergodic_reduction.py %s -i $SLURM_JOB_ID'%("-f '" + str(os.path.join(directory, f)) + "' -d '" + base_directory[:10]+"_"+directory_name + "_reduction'")
     else:
-        params_string = params_string+'&& python3 $(dirname "$path")/simulate_L5PC_ergodic_reduction.py %s -i -1'%("-f '" + str(os.path.join(directory, f)) + "' -d '" + base_directory[:10]+directory_name + "_reduction'")
+        params_string = params_string+'&& python3 $(dirname "$path")/simulate_L5PC_ergodic_reduction.py %s -i -1'%("-f '" + str(os.path.join(directory, f)) + "' -d '" + base_directory[:10]+"_"+directory_name + "_reduction'")
 
     if i%files_per_cpu==files_per_cpu-1 or i==len(onlyfiles)-1:
         pass
