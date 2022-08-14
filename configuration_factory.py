0e886979a2c7d76a70236ea23f265164606fffcf
goolux <30436343+goolux@users.noreply.github.com>
2022-08-14 10:58:59 +0300

    add dropout to fc tcn

--- configuration_factory.py
+++ configuration_factory.py
@@ -59,7 +59,7 @@ def surround_with_default_config_values(**kargs):
                       train_file_load=0.5, valid_file_load=0.5, spike_probability=None,
                       # files_filter_regex=".*exBas_0_1100_inhBasDiff_-1100_600__exApic_0_1100_inhApicDiff_-1100_600_SpTemp[^\\/\.]*\.p",
                       files_filter_regex=".*", freeze_node_factor=None,
-                      optimizer_type="NAdam", optimizer_params=dict(weight_decay=1e-3),clip_gradients_factor=None,  # optimizer_params={'eps':1e-8},
+                      optimizer_type="NAdam", optimizer_params=dict(weight_decay=1e-2),clip_gradients_factor=None,  # optimizer_params={'eps':1e-8},
                       # lr_scheduler='CyclicLR',lr_scheduler_params=dict(max_lr=0.05,step_size_up=1000,base_lr=0.00003,cycle_momentum=True),
                       # lr_scheduler='ReduceLROnPlateau',lr_scheduler_params=dict(factor=0.5,cooldown=300,patience =3000,eps=1e-5, threshold=1e-2),
                       lr_scheduler=None,
