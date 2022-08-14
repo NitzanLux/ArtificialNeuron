65e09e25aa33e7abc9c15216feacd4fb84223060
goolux <30436343+goolux@users.noreply.github.com>
2022-08-14 10:38:37 +0300

    add dropout to fc tcn

--- reduction_simulation_execution.py
+++ reduction_simulation_execution.py
@@ -9,7 +9,7 @@ import json
 from slurm_job import *
 from math import ceil
 from art import tprint
-
+from simulate_L5PC_ergodic_reduction import get_dir_name_and_filename
 parser = argparse.ArgumentParser(description='Add configuration file')
 
 parser.add_argument(dest="number_of_cpus", type=int,
@@ -17,42 +17,48 @@ parser.add_argument(dest="number_of_cpus", type=int,
 
 parser.add_argument(dest="directory", type=str,
                     help='data directory', default=None)
+parser.add_argument(dest="files_that_do_not_exist", type=bool,
+                    help='simulate only files that do not exist', default=False)
 
 args = parser.parse_args()
 print(args)
 
 number_of_cpus = args.number_of_cpus
 directory = args.directory
-onlyfiles = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+only_files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
 # number_of_cpus = len(onlyfiles)
 
+
+
 job_factory = SlurmJobFactory("cluster_logs")
-files_per_cpu = ceil(len(onlyfiles)/number_of_cpus)
+files_per_cpu = ceil(len(only_files) / number_of_cpus)
 
 
 directory_name=os.path.dirname(directory)
 directory_name=os.path.basename(directory_name)
 
-params_string=''
-# for i,f in enumerate(onlyfiles):
-#     if i%number_of_cpus==0:
-#         params_string = " '" + str(os.path.join(directory, f)) + "' -d '" + directory_name + "_reduction'"
-#     else:
-#         params_string = " '" + str(os.path.join(directory, f)) + "'" + params_string
-#     if i%files_per_cpu==files_per_cpu-1 or i==len(onlyfiles)-1:
-#
-#         job_factory.send_job("%s_%s"%("reduction_simulation",directory_name),
-#                          'python3 $(dirname "$path")/simulate_L5PC_reduction_and_create_dataset.py %s -i $SLURM_JOB_ID' % ("-f"+params_string),filename_index=i//files_per_cpu)
-print(len(onlyfiles),flush=True)
 
-for i,f in enumerate(onlyfiles):
+if args.files_that_do_not_exist:
+    directory_dest , _ = get_dir_name_and_filename("",directory_name)
+    files_that_exists = set([f for f in os.listdir(directory_dest) if os.path.isfile(os.path.join(directory_dest, f))])
+    new_files=[]
+    for f in only_files:
+        if f not in files_that_exists:
+            new_files.append(f)
+    only_files = new_files
+
+params_string=''
+
+print(len(only_files), flush=True)
+
+for i,f in enumerate(only_files):
     print(directory_name)
     if i%files_per_cpu==0:
         params_string = 'python3 $(dirname "$path")/simulate_L5PC_reduction_and_create_dataset.py %s -i $SLURM_JOB_ID'%("-f '" + str(os.path.join(directory, f)) + "' -d '" + directory_name + "_reduction'")
     else:
         params_string = params_string+'&& python3 $(dirname "$path")/simulate_L5PC_reduction_and_create_dataset.py %s -i -1'%("-f '" + str(os.path.join(directory, f)) + "' -d '" + directory_name + "_reduction'")
 
-    if i%files_per_cpu==files_per_cpu-1 or i==len(onlyfiles)-1:
+    if i%files_per_cpu==files_per_cpu-1 or i==len(only_files)-1:
         pass
         # job_factory.send_job("%s_%s"%("reduction_simulation",directory_name), params_string,filename_index=i//files_per_cpu)
 
