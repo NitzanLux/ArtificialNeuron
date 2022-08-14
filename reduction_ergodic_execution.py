ef26a054affed17da7d33c0ce4a70e62f76705b1
goolux <30436343+goolux@users.noreply.github.com>
2022-08-14 11:24:11 +0300

    add dropout to fc tcn

--- reduction_ergodic_execution.py
+++ reduction_ergodic_execution.py
@@ -19,7 +19,7 @@ parser.add_argument(dest="directory", type=str,
 parser.add_argument(dest="files_that_do_not_exist", type=bool,
                     help='simulate only files that do not exist', default=False)
 parser.add_argument(dest="reduction_frequency", type=int,
-                    help='reduction frequency', default= 0)
+                    help='reduction frequency', default=0)
 
 args = parser.parse_args()
 print(args)
@@ -32,15 +32,14 @@ only_files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(di
 job_factory = SlurmJobFactory("cluster_logs")
 files_per_cpu = ceil(len(only_files) / number_of_cpus)
 
-base_directory=os.path.dirname(directory)
-directory_name=os.path.basename(directory)
-base_directory=os.path.basename(base_directory)
-
+base_directory = os.path.dirname(directory)
+directory_name = os.path.basename(directory)
+base_directory = os.path.basename(base_directory)
 
 if args.files_that_do_not_exist:
-    directory_dest  = os.path.join(NEURON_REDUCE_DATA_DIR, base_directory+"_"+directory_name + "_reduction")
+    directory_dest = os.path.join(NEURON_REDUCE_DATA_DIR, base_directory + "_" + directory_name + "_reduction")
     files_that_exists = set([f for f in os.listdir(directory_dest) if os.path.isfile(os.path.join(directory_dest, f))])
-    new_files=[]
+    new_files = []
     for f in only_files:
         file_name, file_extension = os.path.splitext(f)
         _, file_name = os.path.split(file_name)
@@ -49,18 +48,21 @@ if args.files_that_do_not_exist:
             new_files.append(f)
     only_files = new_files
 
-
-params_string=''
+params_string = ''
 
 print(len(only_files), flush=True)
 
-for i,f in enumerate(only_files):
-    if i%files_per_cpu==0:
-        params_string = 'python3 $(dirname "$path")/simulate_L5PC_ergodic_reduction.py %s -i $SLURM_JOB_ID'%("-f '" + str(os.path.join(directory, f)) + "' -d '" + base_directory+"_"+directory_name + "_reduction'"+" -rf "+args.reduction_frequency)
+for i, f in enumerate(only_files):
+    if i % files_per_cpu == 0:
+        params_string = 'python3 $(dirname "$path")/simulate_L5PC_ergodic_reduction.py %s -i $SLURM_JOB_ID' % (
+                    "-f '" + str(os.path.join(directory,
+                                              f)) + "' -d '" + base_directory + "_" + directory_name + "_reduction'" +
+                    " -rf %d"%args.reduction_frequency)
     else:
-        params_string = params_string+'&& python3 $(dirname "$path")/simulate_L5PC_ergodic_reduction.py %s -i -1'%("-f '" + str(os.path.join(directory, f)) + "' -d '" + base_directory+"_"+directory_name + "_reduction'"+" -rf "+args.reduction_frequency)
+        params_string = params_string + '&& python3 $(dirname "$path")/simulate_L5PC_ergodic_reduction.py %s -i -1' % (
+                    "-f '" + str(os.path.join(directory,
+                                              f)) + "' -d '" + base_directory + "_" + directory_name + "_reduction'" +
+                    " -rf %d"%args.reduction_frequency)
 
     # if i%files_per_cpu==files_per_cpu-1 or i==len(only_files)-1:
     #     job_factory.send_job("%s_%s"%("reduction_simulation",base_directory[:15]+"_"+directory_name), params_string,filename_index=i//files_per_cpu)
-
-
