a5844dd49009ca12b259ac7784a745640e6eedf6
goolux <30436343+goolux@users.noreply.github.com>
2022-08-15 15:28:00 +0300

    add dropout to fc tcn

--- fit_CNN.py
+++ fit_CNN.py
@@ -48,15 +48,15 @@ synapse_type = 'NMDA'
 include_DVT = False
 print_logs=False
 # for dibugging
-print_logs=True
-os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
-NUMBER_OF_HOURS_FOR_SAVING_MODEL_AND_CONFIG=0.5
-BATCH_LOG_UPDATE_FREQ = 1
-VALIDATION_EVALUATION_FREQUENCY=4
-ACCURACY_EVALUATION_FREQUENCY = 4
-BUFFER_SIZE_IN_FILES_VALID = 1
-BUFFER_SIZE_IN_FILES_TRAINING = 1
-DOCUMENT_ON_WANDB = False
+# print_logs=True
+# os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+# NUMBER_OF_HOURS_FOR_SAVING_MODEL_AND_CONFIG=0.5
+# BATCH_LOG_UPDATE_FREQ = 1
+# VALIDATION_EVALUATION_FREQUENCY=4
+# ACCURACY_EVALUATION_FREQUENCY = 4
+# BUFFER_SIZE_IN_FILES_VALID = 1
+# BUFFER_SIZE_IN_FILES_TRAINING = 1
+# DOCUMENT_ON_WANDB = False
 print('-----------------------------------------------')
 print('finding data')
 print('-----------------------------------------------', flush=True)
