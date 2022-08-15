303b7ab938c5f90a8ab5b75286ee34b0276e8cdf
goolux <30436343+goolux@users.noreply.github.com>
2022-08-15 19:11:00 +0300

    ido data run

--- general_aid_function.py
+++ general_aid_function.py
@@ -28,15 +28,15 @@ def filter_file_names(files: List[str], filter: str) -> List[str]:
 
 
 def load_files_names(files_filter_regex: str = ".*",ido_format=True) -> Tuple[List[str], List[str], List[str]]:
-    train_files =  glob.glob(TRAIN_DATA_DIR + '*'+'/' if ido_format else "")
+    train_files =  glob.glob(TRAIN_DATA_DIR + '*'+('/' if ido_format else ""))
     print(train_files)
     train_files = filter_file_names(train_files, files_filter_regex)
     print("train_files size %d" % (len(train_files)))
-    valid_files = glob.glob(VALID_DATA_DIR + '*'+'/' if ido_format else "")
+    valid_files = glob.glob(VALID_DATA_DIR + '*'+('/' if ido_format else ""))
     valid_files = filter_file_names(valid_files, files_filter_regex)
     print("valid_files size %d" % (len(valid_files)))
 
-    test_files = glob.glob(TEST_DATA_DIR + '*'+'/' if ido_format else "")
+    test_files = glob.glob(TEST_DATA_DIR + '*'+('/' if ido_format else ""))
     test_files = filter_file_names(test_files, files_filter_regex)
     print("test_files size %d" % (len(test_files)))
 
