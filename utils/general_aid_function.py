cfa210aaf39d1a2e952a1a41740f74fc347ceb8a
goolux <30436343+goolux@users.noreply.github.com>
2022-08-18 16:06:57 +0300

    update packages

--- utils/general_aid_function.py
+++ utils/general_aid_function.py
@@ -3,7 +3,7 @@ import os
 from project_path import *
 from typing import List, Tuple
 import re
-import neuron_network.node_network.recursive_neuronal_model as recursive_neuronal_model
+# import neuron_network.node_network.recursive_neuronal_model as recursive_neuronal_model
 import platform
 
 NEW_DIR_SIGN = '\\' if platform.system() == 'Windows'else '/'
@@ -14,13 +14,13 @@ class AttrDict(dict):
         super(AttrDict, self).__init__(*args, **kwargs)
         self.__dict__ = self
 
-def load_model(config):
-    print("loading model...", flush=True)
-    if "network_architecture_structure" in config and config.network_architecture_structure == "recursive":
-        model = recursive_neuronal_model.RecursiveNeuronModel.load(config)
-    else:
-        assert False,"cannot import model"
-    return model
+# def load_model(config):
+#     print("loading model...", flush=True)
+#     if "network_architecture_structure" in config and config.network_architecture_structure == "recursive":
+#         model = recursive_neuronal_model.RecursiveNeuronModel.load(config)
+#     else:
+#         assert False,"cannot import model"
+#     return model
 
 def filter_file_names(files: List[str], filter: str) -> List[str]:
     compile_filter = re.compile(filter)
