edce373e778d29416ffab9eff8fed2b9aa55703e
goolux <30436343+goolux@users.noreply.github.com>
2022-08-23 11:32:33 +0300

    update packages

--- model_evaluation_script.py
+++ model_evaluation_script.py
@@ -9,6 +9,8 @@ from utils.slurm_job import *
 from math import ceil
 import json
 from model_evaluation_multiple import create_gt_and_save
+from pathlib import Path
+
 parser = argparse.ArgumentParser(description='Add gt path and name')
 parser.add_argument(dest="gt_name", type=str,
                     help='gt name', default=None)
@@ -16,10 +18,11 @@ parser.add_argument(dest="model_name", type=str,
                     help='model name', default=None)
 job_factory = SlurmJobFactory("cluster_logs")
 args = parser.parse_args()
-
+gt_name = str(Path(os.path.basename(args.gt_name)).with_suffix(''))
+model_name = str(Path(os.path.basename(args.model_name)).with_suffix(''))
 print(args)
 
-a = job_factory.send_job('model_%s'%args.gt_name,'python -c "from model_evaluation_multiple import create_model_evaluation; create_model_evaluation(%s,%s)"'%("'"+args.gt_name+"'","'"+args.model_name+"'"))
+a = job_factory.send_job('model_%s'%gt_name,'python -c "from model_evaluation_multiple import create_model_evaluation; create_model_evaluation(%s,%s)"'%("'"+gt_name+"'","'"+model_name+"'"), run_on_GPU=True)
 print(a)
 
 
