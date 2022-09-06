e37d6f8935db94b261adde8b8456e63eed96a820
goolux <30436343+goolux@users.noreply.github.com>
2022-09-06 17:26:58 +0300

    update epochs

--- train_nets/configuration_factory.py
+++ train_nets/configuration_factory.py
@@ -289,6 +289,7 @@ if __name__ == '__main__':
                 constant_learning_rate=0.0007)
             configs.append(config)
         # configs.extend(generate_config_files_multiple_seeds(config_morpho_0, 2))
+
     print(configurations_name)
     with open(os.path.join(MODELS_DIR, "%s.json" % configurations_name), 'w') as file:
         file.write(json.dumps(configs))  # use `json.loads` to do the reverse
