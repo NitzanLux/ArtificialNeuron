fbfbe6e24ac3bbb7c3ff1eaf5ef3fa72ac9963f9
goolux <30436343+goolux@users.noreply.github.com>
2022-09-08 08:43:52 +0300

    added multiprocessing and savemodel flag

--- train_nets/fit_CNN.py
+++ train_nets/fit_CNN.py
@@ -557,7 +557,7 @@ def run_fit_cnn():
     # send_mail("nitzan.luxembourg@mail.huji.ac.il","somthing went wrong",e)
     # raise e
 
-if __name__ == "__main__":
-    run_fit_cnn()
+# if __name__ == "__main__":
+run_fit_cnn()
 
 # send_mail("nitzan.luxembourg@mail.huji.ac.il","finished run","finished run")
