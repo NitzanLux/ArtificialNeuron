23bb19c4a8fd244246c047134336ee3545050aab
goolux <30436343+goolux@users.noreply.github.com>
2022-09-12 15:19:29 +0300

    added multiprocessing and savemodel flag

--- utils/general_variables.py
+++ utils/general_variables.py
@@ -1,5 +1,5 @@
 import torch
 
-USE_CUDA = torch. cuda. is_available()
-DATA_TYPE = torch.cuda.DoubleTensor
-# DATA_TYPE = torch.cuda.FloatTensor
+USE_CUDA = torch.cuda.is_available()
+DATA_TYPE = torch.cuda.DoubleTensor if USE_CUDA else torch.DoubleTensor
+# DATA_TYPE = torch.cuda.FloatTensor if USE_CUDA else torch.FloatTensor
