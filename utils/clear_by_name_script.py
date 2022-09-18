39eb7e885ffe3e58e9d25046a96fe759393b5d16
goolux <30436343+goolux@users.noreply.github.com>
2022-09-18 11:32:20 +0300

    added remove non linearity and regularization in last layer

---
+++ utils/clear_by_name_script.py
@@ -0,0 +1,22 @@
+import time
+import os
+import logging
+from project_path import *
+import argparse
+import json
+from utils.slurm_job import *
+import subprocess
+
+parser = argparse.ArgumentParser(description='json file...')
+
+parser.add_argument(dest="job_name_format", type=str,
+                    help='configurations json file of paths', default=None)
+parser.add_argument(dest="amount", type=int,
+                    help='number_of_jobs', default=None)
+
+result = subprocess.run(['squeue', '--me','-o' ,'"%.1i %.1P %100j %1T %.1M  %.R"'], stdout=subprocess.PIPE)
+print(result)
+# number_of_jobs=len(configs)
+
+
+print(args)
