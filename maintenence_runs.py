99d1bd03486b20abbea6a7e981e2fdef37bf7846
goolux <30436343+goolux@users.noreply.github.com>
2022-09-18 14:29:47 +0300

    added remove non linearity and regularization in last layer

--- maintenence_runs.py
+++ maintenence_runs.py
@@ -7,7 +7,7 @@ runs_array=[
             "python fit_CNN_execution.py d_r_comparison_ss -g False -mem 120000",
             "python fit_CNN_execution.py morph -g True",
             "python fit_CNN_execution.py morph_linear -g True",
-            ]+["python evaluation_datasets -j d_r_comparison -j d_r_comparison_ss -j morph -j morph_linear"]
+            ]+["python evaluation_datasets.py -j d_r_comparison -j d_r_comparison_ss -j morph -j morph_linear"]
 
 for i,s in enumerate(runs_array):
     print(f"Now running command: {s}")
