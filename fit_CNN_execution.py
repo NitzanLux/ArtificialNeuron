4f95e08101b813e4ad1d8224823226b87cb96060
goolux <30436343+goolux@users.noreply.github.com>
2022-09-18 10:59:51 +0300

    added remove non linearity and regularization in last layer

--- fit_CNN_execution.py
+++ fit_CNN_execution.py
@@ -10,7 +10,8 @@ parser = argparse.ArgumentParser(description='Add configuration file')
 
 parser.add_argument(dest="configs_paths", type=str,
                     help='configurations json file of paths', default=None)
-
+parser.add_argument(dest="use_gpu", type=bool,
+                    help='true if to use gpu false otherwise', default=False)
 args = parser.parse_args()
 print(args)
 
@@ -22,5 +23,5 @@ with open(os.path.join(MODELS_DIR, "%s.json" % configs_file), 'r') as file:
 for i, conf in enumerate(configs):
     job_factory.send_job("%i_%s" % (i, configs_file),
                          'python3 -m train_nets.fit_CNN %s $SLURM_JOB_ID' % str(
-                             os.path.join(MODELS_DIR, *conf)),True)
+                             os.path.join(MODELS_DIR, *conf)),args.use_gpu)
                             # , False,mem=120000)
