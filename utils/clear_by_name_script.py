e0f4d82587e5880d91d514a71f1a40adaa5ca6ec
goolux <30436343+goolux@users.noreply.github.com>
2022-09-18 11:52:38 +0300

    added remove non linearity and regularization in last layer

--- utils/clear_by_name_script.py
+++ utils/clear_by_name_script.py
@@ -15,7 +15,7 @@ parser.add_argument(dest="amount", type=int,
                     help='number_of_jobs', default=None)
 
 result = subprocess.run(['squeue', '--me','-o' ,'"%.1i %.1P %100j %1T %.1M  %.R"'], stdout=subprocess.PIPE)
-result = str(result.stdout).decode('utf-8')
+result = result.stdout.decode('utf-8')
 print(str(result))
 
 # print("****")
