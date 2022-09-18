5be20aea48aca339e72416da7fa26af8c99a9195
goolux <30436343+goolux@users.noreply.github.com>
2022-09-18 14:24:31 +0300

    added remove non linearity and regularization in last layer

--- maintenence_runs.py
+++ maintenence_runs.py
@@ -11,6 +11,6 @@ runs_array=[
 
 for i,s in enumerate(runs_array):
     print(f"Now running command: {s}")
-    s=['printf','y']+re.split(f"[\s]+",s)
+    s=['yes']+re.split(f"[\s]+",s)
     result = subprocess.run(s, stderr=sys.stderr, stdout=sys.stdout)
     time.sleep(5)
