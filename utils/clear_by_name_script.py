34d521b573706a4371bfc5c6f8da95bf7ea316fa
goolux <30436343+goolux@users.noreply.github.com>
2022-09-18 12:04:08 +0300

    added remove non linearity and regularization in last layer

--- utils/clear_by_name_script.py
+++ utils/clear_by_name_script.py
@@ -11,9 +11,8 @@ parser = argparse.ArgumentParser(description='json file...')
 
 parser.add_argument(dest="job_name_format", type=str,
                     help='configurations json file of paths', default=None)
-parser.add_argument(dest="amount", type=int,
-                    help='number_of_jobs', default=None)
 
+args = parser.parse_args()
 result = subprocess.run(['squeue', '--me','-o' ,'"%.1i %.1P %100j %1T %.1M  %.R"'], stdout=subprocess.PIPE)
 result = result.stdout.decode('utf-8')
 result=str(result)
@@ -21,10 +20,13 @@ result = result.split('\n')
 for i,s in enumerate(result):
     result[i] = re.split('[\s]+',s)
 index = result[0].index("NAME")
+
+m=re.compile(f"{args.job_name_format}")
 for i,arr in enumerate(result):
     if i == 0 or len(arr)<index+1:
         continue
-    print(arr[index])
+    if m.match(arr[index]):
+        print(arr[index])
 
 
 
