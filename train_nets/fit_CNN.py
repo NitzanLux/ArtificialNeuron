a393fafe6360170a268f4a68f5d9a318fa66cee1
goolux <30436343+goolux@users.noreply.github.com>
2022-09-19 17:59:41 +0300

    add argument and maintenence runs

--- train_nets/fit_CNN.py
+++ train_nets/fit_CNN.py
@@ -463,7 +463,8 @@ def load_optimizer(config, model):
         with open(os.path.join(MODELS_DIR, *config.model_path) + '.optim', 'rb') as f:
             state_dict = pickle.load(f)
         optimizer.load_state_dict(state_dict)
-        optimizer.param_groups[0]=config.optimizer_params['lr']
+        print(type(optimizer.param_groups[0]))
+        # optimizer.param_groups[0]=config.optimizer_params['lr']
     return optimizer
 
 
