284b0af3b627d35446578ad0086f419bec70620a
goolux <30436343+goolux@users.noreply.github.com>
2022-09-19 18:25:40 +0300

    add argument and maintenence runs

--- utils/general_variables.py
+++ utils/general_variables.py
@@ -2,4 +2,5 @@ import torch
 
 USE_CUDA = torch.cuda.is_available()
 DATA_TYPE = torch.cuda.DoubleTensor if USE_CUDA else torch.DoubleTensor
+DATA_TYPE_TENSOR =  torch.float64
 # DATA_TYPE = torch.cuda.FloatTensor if USE_CUDA else torch.FloatTensor
