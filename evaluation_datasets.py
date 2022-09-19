38c244a43ee040d62708e7ccc565b17a489e244d
goolux <30436343+goolux@users.noreply.github.com>
2022-09-19 13:09:25 +0300

    add argument and maintenence runs

--- evaluation_datasets.py
+++ evaluation_datasets.py
@@ -1,9 +1,10 @@
 from model_evaluation_multiple import *
 from utils.slurm_job import *
 from project_path import *
+from utils.general_aid_function import get_works_on_cluster
 import platform
 import argparse
-
+import logging
 if __name__ == '__main__':
     if platform.system() == 'Windows':
         run_test()
@@ -43,21 +44,12 @@ if __name__ == '__main__':
                                                     ' create_model_evaluation(%s,%s)"'%("'" + gt_name + "'", "'" + i + "'") )
         number_of_jobs=min(number_of_jobs, len(commands))
         jumps= len(commands)//number_of_jobs
+        if len(get_works_on_cluster("model_evaluations_[0-9]+"))>0:
+            logging.warning("evaluation is already in progress, please wait")
+            exit(0)
         for c,i in enumerate(range(0, len(commands), jumps)):
             command=" && ".join(commands[i:min(i+jumps,len(commands))])
             keys={}
             if not use_gpu:
                 keys={'mem':120000}
             job_factory.send_job(f'model_evaluations_{c}',command, run_on_GPU=use_gpu,**keys)
-
-    # for i in ["morph_7___2022-09-07__23_01__ID_42876",
-    #             "morph_7_reduction___2022-09-07__23_01__ID_28654",
-    #             "d_r_comparison_7___2022-09-07__22_59__ID_57875",
-    #             "morph_linear_7___2022-09-15__17_55__ID_30822",
-    #             "d_r_comparison_7_reduction___2022-09-07__22_59__ID_31437",
-    #             "d_r_comparison_5___2022-09-07__22_59__ID_65381",
-    #             "d_r_comparison_5_reduction___2022-09-07__22_59__ID_9020",
-    #             "d_r_comparison_3___2022-09-07__22_59__ID_53410",
-    #             "d_r_comparison_3_reduction___2022-09-07__22_59__ID_44648",
-    #             "d_r_comparison_1___2022-09-07__22_59__ID_14835",
-    #             "d_r_comparison_1_reduction___2022-09-07__22_59__ID_14945"]:
