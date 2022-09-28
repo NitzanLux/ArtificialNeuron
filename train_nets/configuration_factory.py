4ad352f418973d64694bcdc4c4871b0f850c13a3
goolux <30436343+goolux@users.noreply.github.com>
2022-09-28 14:36:03 +0300

    add argument and maintenence runs

--- train_nets/configuration_factory.py
+++ train_nets/configuration_factory.py
@@ -135,7 +135,7 @@ def load_config_file(path: str) -> AttrDict:
     # config.prediction_length = (6000 - 600) // 4
     # config.
     congif.lr_scheduler='ReduceLROnPlateau'
-    config.lr_scheduler_params=dict(factor=0.5,cooldown=500,patience =5000,eps=1e-7),
+    config.lr_scheduler_params=dict(factor=0.5,cooldown=500,patience =3000,eps=1e-7),
     # config.lr_scheduler_params = dict(factor=0.5, cooldown=500, threshold=1e-2, patience=1000, eps=1e-5)
     # config.lr_scheduler_params=dict()
     # config.lr_scheduler=None
