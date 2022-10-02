ccda9b9d2dd6598dd6211c9fb035a46ac5709d79
goolux <30436343+goolux@users.noreply.github.com>
2022-10-02 12:38:44 +0300

    add argument and maintenence runs

--- maintenence_runs.py
+++ maintenence_runs.py
@@ -19,7 +19,7 @@ runs_array=[
             # rf"python -c ' {rf'from maintenence_runs import reduce_lr; reduce_lr({str(models_jsons)})'} '",
             # r'python evaluation_datasets.py -j d_r_comparison -j d_r_comparison_ss -j morph  -n 15 -g False']+[
             r"python fit_CNN_execution.py d_r_comparison -g True",
-            r"python fit_CNN_execution.py d_r_comparison_ss -g False -mem 160000",
+            r"python fit_CNN_execution.py d_r_comparison_ss -g False -mem 200000",
             r"python fit_CNN_execution.py morph -g True",
             r"python fit_CNN_execution.py morph_linear -g True",
             ]
