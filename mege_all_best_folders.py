f0a94bf312715c7c30137d8caafe9d8d72cc3835
goolux <30436343+goolux@users.noreply.github.com>
2022-10-02 12:01:06 +0300

    add argument and maintenence runs

---
+++ mege_all_best_folders.py
@@ -0,0 +1,20 @@
+import os
+import  shutil
+from project_path import *
+for i in os.listdir(MODELS_DIR):
+    if not os.path.isdir(i):
+        continue
+    best_arr=[]
+    creat_folder_flag=True
+    for f in os.listdir(i):
+        if 'best' in f and 'temp' in f:
+            best_arr.append(f)
+        elif 'best' in f and 'temp' not in f:
+            creat_folder_flag=False
+    if len(best_arr)==0:
+        continue
+    dest=best_arr[0].replace('temp','')
+    if creat_folder_flag:
+        os.path.mkdir(os.path.join(MODELS_DIR,i,dest))
+    for f in best_arr:
+        shutil.move(os.path.join(MODELS_DIR,i,f),os.path.join(MODELS_DIR,i,dest))
