c9095e73099ee85dc4524cd4baf7758bae6b77ac
goolux <30436343+goolux@users.noreply.github.com>
2022-10-02 12:13:08 +0300

    add argument and maintenence runs

--- mege_all_best_folders.py
+++ mege_all_best_folders.py
@@ -8,18 +8,28 @@ if __name__ == '__main__':
             continue
         best_arr=[]
         creat_folder_flag=True
-        for f in os.listdir(os.path.join(MODELS_DIR,i)):
-            if 'best' in f and 'temp' in f:
-                print(f)
 
-                best_arr.append(f)
-            elif 'best' in f and 'temp' not in f:
-                creat_folder_flag=False
-        if len(best_arr)==0:
-            continue
-        dest=best_arr[0].replace('temp','')
-        if creat_folder_flag:
-            os.path.mkdir(os.path.join(MODELS_DIR,i,dest))
-        for f in best_arr:
-            print(dest)
-            shutil.move(os.path.join(MODELS_DIR,i,f),os.path.join(MODELS_DIR,i,dest))
+        for f in os.listdir(os.path.join(MODELS_DIR,i)):
+            if 'best' in f:
+                for k in os.listdir(os.path.join(MODELS_DIR,i,f)):
+                    cur_path = os.path.join(MODELS_DIR,i,f,k)
+                    if os.path.isdir(cur_path):
+                        for h in os.listdir(cur_path):
+                            shutil.move(cur_path,os.path.join(MODELS_DIR, i, f))
+                        if len(os.listdir(cur_path))==0:
+                            os.remove(cur_path)
+        # for f in os.listdir(os.path.join(MODELS_DIR,i)):
+        #     if 'best' in f and 'temp' in f:
+        #         print(f)
+        #
+        #         best_arr.append(f)
+        #     elif 'best' in f and 'temp' not in f:
+        #         creat_folder_flag=False
+        # if len(best_arr)==0:
+        #     continue
+        # dest=best_arr[0].replace('temp','')
+        # if creat_folder_flag:
+        #     os.path.mkdir(os.path.join(MODELS_DIR,i,dest))
+        # for f in best_arr:
+        #     print(dest)
+        #     shutil.move(os.path.join(MODELS_DIR,i,f),os.path.join(MODELS_DIR,i,dest))
