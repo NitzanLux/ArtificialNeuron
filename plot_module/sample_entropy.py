0c9fc551c3a0404e61f5ab0017b606275b82be12
goolux <30436343+goolux@users.noreply.github.com>
2022-10-10 13:55:55 +0300

    working on comparison plots

--- plot_module/sample_entropy.py
+++ plot_module/sample_entropy.py
@@ -10,7 +10,7 @@ import argparse
 from multiprocessing import Process,Queue
 number_of_cpus = multiprocessing.cpu_count()
 import queue
-MAX_INTERVAL = 50
+MAX_INTERVAL = 40
 print("start job")
 number_of_jobs=number_of_cpus-1
 def create_sample_entropy_file(q):
