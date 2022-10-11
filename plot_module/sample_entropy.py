4badf303c3b4fbb98ee4e9ec2e3388abf3c3a9b8
goolux <30436343+goolux@users.noreply.github.com>
2022-10-11 17:16:41 +0300

    remove pause and retry from schedualer

--- plot_module/sample_entropy.py
+++ plot_module/sample_entropy.py
@@ -10,7 +10,7 @@ import argparse
 from multiprocessing import Process,Queue
 number_of_cpus = multiprocessing.cpu_count()
 import queue
-MAX_INTERVAL = 20
+MAX_INTERVAL = 5
 print("start job")
 number_of_jobs=number_of_cpus//5
 def create_sample_entropy_file(q):
@@ -20,8 +20,8 @@ def create_sample_entropy_file(q):
             t = time.time()
             se_r, _, _ = EH.SampEn(sr, m=MAX_INTERVAL)
             se_o, _, _ = EH.SampEn(so, m=MAX_INTERVAL)
-            np.save(os.path.join("sample_entropy",f"sample_entropy_reduction_{i}_{MAX_INTERVAL}d.npy"), np.array(se_r))
-            np.save(os.path.join("sample_entropy",f"sample_entropy_original_{i}_{MAX_INTERVAL}d.npy"), np.array(se_o))
+            np.save(os.path.join("..","sample_entropy",f"sample_entropy_reduction_{i}_{MAX_INTERVAL}d.npy"), np.array(se_r))
+            np.save(os.path.join("..","sample_entropy",f"sample_entropy_original_{i}_{MAX_INTERVAL}d.npy"), np.array(se_o))
             print(
                 f"current sample number {i}   total: {time.time() - t} seconds",
                 flush=True)
