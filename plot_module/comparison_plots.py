3d0070470d5a1945442fd88a09f7711ff31e350a
goolux <30436343+goolux@users.noreply.github.com>
2022-10-11 16:15:44 +0300

    remove pause and retry from schedualer

--- plot_module/comparison_plots.py
+++ plot_module/comparison_plots.py
@@ -73,8 +73,8 @@ for i,m in enumerate(model_reduction_names):
         max_layer=m.config.number_of_layers_space
     if use_custom_threshold:
         fpr, tpr, thresholds = skm.roc_curve(reduction_output_s, s)
-        optimal_idx = np.argmax(tpr - fpr)
-        optimal_threshold = thresholds[optimal_idx]*np.sum(reduction_output_s)/reduction_output_s.shape[0]
+        optimal_idx = np.argmax(tpr*np.sum(reduction_output_s) - fpr*reduction_output_s.shape[0])
+        optimal_threshold = thresholds[optimal_idx]
     else:
         optimal_threshold = m.get_ROC_data()[3]*np.sum(reduction_output_s)/reduction_output_s.shape[0]
     model_evaluation_reduction.append((v,s,m.config.number_of_layers_space,optimal_threshold))
