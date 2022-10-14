130dfd6f7de66211af02d2a98a13fbaf7d32376d
goolux <30436343+goolux@users.noreply.github.com>
2022-10-14 20:08:57 +0300

    update save_best_model without first_run

--- plot_module/sample_entropy.py
+++ plot_module/sample_entropy.py
@@ -79,7 +79,7 @@ if __name__ == "__main__":
     #                     help='set memory', default=-1)
     # args = parser.parse_args()
     from utils.slurm_job import *
-    number_of_clusters=10
+    number_of_clusters=30
     job_factory = SlurmJobFactory("cluster_logs")
     gt_reduction_name = 'reduction_ergodic_validation'
     size = len(GroundTruthData.load(os.path.join( 'evaluations', 'ground_truth', gt_reduction_name + '.gteval')))
