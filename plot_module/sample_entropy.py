b2badae3884b66985c93786318ebc3ffb8271808
goolux <30436343+goolux@users.noreply.github.com>
2022-10-15 17:27:34 +0300

    update save_best_model without first_run

--- plot_module/sample_entropy.py
+++ plot_module/sample_entropy.py
@@ -15,12 +15,34 @@ print("start job")
 import pickle as pickle
 number_of_jobs=number_of_cpus-1//5
 number_of_jobs=1
-def create_sample_entropy_file(q):
+def create_sample_entropy_file(q,use_process=True):
 
     # gt_original_name = 'davids_ergodic_train'
     # gt_reduction_name = 'reduction_ergodic_train'
     # gt_reduction = GroundTruthData.load(os.path.join( 'evaluations', 'ground_truth', gt_reduction_name + '.gteval'))
     # gt_original = GroundTruthData.load(os.path.join('evaluations', 'ground_truth', gt_original_name + '.gteval'))
+    if not use_process:
+        sr, so, index, r_tag,o_tag=data
+        # r, o = gt_reduction.get_by_index(index), gt_original.get_by_index(index)
+        # vr, sr = r
+        # vo, so = o
+        sr= sr.astype(np.float64)
+        so= so.astype(np.float64)
+        t = time.time()
+        r_Mobj = EH.MSobject('SampEn', m=2,tau =1)
+        o_Mobj = EH.MSobject('SampEn', m=2,tau =1)
+        r_MSx, r_Ci = EH.MSEn(sr, r_Mobj, Scales=MAX_INTERVAL)
+        o_MSx, o_Ci = EH.MSEn(so, o_Mobj, Scales=MAX_INTERVAL)
+        # se_r, _, _ = EH.SampEn(sr, m=MAX_INTERVAL)
+        # se_o, _, _ = EH.SampEn(so, m=MAX_INTERVAL)
+        print(
+            f"current sample number {i}   total: {time.time() - t} seconds",
+            flush=True)
+        with open(os.path.join("sample_entropy",f"sample_entropy_{r_tag}_{i}_{MAX_INTERVAL}d.p"),'wb') as f:
+            pickle.dump((r_MSx,r_Ci),f)
+        with open(os.path.join("sample_entropy",f"sample_entropy_{o_tag}_{i}_{MAX_INTERVAL}d.p"),'wb') as f:
+            pickle.dump((o_MSx,o_Ci),f)
+        return
     while True:
         if q.empty():
             return
@@ -59,20 +81,24 @@ def get_sample_entropy(indexes:[int,List[int]]):
     gt_reduction_name = 'reduction_ergodic_train'
     gt_reduction = GroundTruthData.load(os.path.join( 'evaluations', 'ground_truth', gt_reduction_name + '.gteval'))
     gt_original = GroundTruthData.load(os.path.join('evaluations', 'ground_truth', gt_original_name + '.gteval'))
-
-    queue=Queue(maxsize=number_of_jobs)
-    process = [Process(target=create_sample_entropy_file, args=(queue,)) for i in range(number_of_jobs)]
+    if number_of_jobs>1:
+        queue=Queue(maxsize=number_of_jobs)
+        process = [Process(target=create_sample_entropy_file, args=(queue,)) for i in range(number_of_jobs)]
 
     for j,index in enumerate(indexes):
         r,o = gt_reduction.get_by_index(index), gt_original.get_by_index(index)
         vr, sr = r
         vo, so = o
-        queue.put((sr,so,index,gt_reduction_name,gt_original_name))
-        if j<len(process):
-            process[j].start()
+        if number_of_jobs>1:
+            queue.put((sr,so,index,gt_reduction_name,gt_original_name))
+            if j<len(process):
+                process[j].start()
+        else:
+            create_sample_entropy_file((sr,so,index,gt_reduction_name,gt_original_name),False)
 
-    for p in process:
-        p.join()
+    if number_of_jobs>1:
+        for p in process:
+            p.join()
 
 
 if __name__ == "__main__":
