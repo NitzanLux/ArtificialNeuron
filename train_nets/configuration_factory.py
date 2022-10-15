fc3a32bfef5afa3d4120ddb8e959af7435bbadf5
goolux <30436343+goolux@users.noreply.github.com>
2022-10-16 01:24:21 +0300

    update save_best_model without first_run

--- train_nets/configuration_factory.py
+++ train_nets/configuration_factory.py
@@ -343,7 +343,7 @@ def arange_kernel_by_layers(kernels, layers, expend=False):
 if __name__ == '__main__':
     # restore_last_n_configs(100)
     configs = []
-    configurations_name = "comparison_fixed"
+    configurations_name = "comparison_corrected"
     # configurations_name = 'morph'
     base_layer = [54] + [12] * 6
     for k in range(1):
@@ -356,7 +356,7 @@ if __name__ == '__main__':
                     # architecture_type='LAYERED_TEMPORAL_CONV_N', clip_gradients_factor=2.5,
                     # model_tag="%s_%d%s" % (configurations_name, i, "_reduction" if data == REDUCTION_BASE_PATH else ''),
                     model_tag="%s_%s" % (configurations_name, "_reduction" if data == REDUCTION_BASE_PATH else ''),
-                    kernel_sizes=kernels, number_of_layers_space=len(kernels), data_base_path=data,trim_last_nonlinear=True,
+                    kernel_sizes=kernels, number_of_layers_space=len(kernels), data_base_path=data,#trim_last_nonlinear=True,
                     accumulate_loss_batch_factor=1, prediction_length=700,torch_seed=torch_seed,numpy_seed=numpy_seed,random_seed=random_seed,input_window_size=sum(kernels)-len(kernels)+1,
                     # batch_size_validation=30, batch_size_train=80,
                     # batch_size_validation=30, batch_size_train=5,
