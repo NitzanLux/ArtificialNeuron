f3ceaddf2e5e1bf09bbcf018375ca7512ebff9a9
goolux <30436343+goolux@users.noreply.github.com>
2022-10-16 16:18:13 +0300

    update save_best_model without first_run

--- plot_module/sample_entropy.py
+++ plot_module/sample_entropy.py
@@ -38,7 +38,7 @@ def create_sample_entropy_file(q):
         for index in range(y_spike.shape[0]):
             print(f'start key:{f} index:{index}')
             s = y_spike[index,:].astype(np.float64)
-            print(s)
+            print(s,s.shape)
             t = time.time()
             Mobj = EH.MSobject('SampEn', m=2,tau =1)
             MSx, Ci = EH.MSEn(s, Mobj, Scales=MAX_INTERVAL)
