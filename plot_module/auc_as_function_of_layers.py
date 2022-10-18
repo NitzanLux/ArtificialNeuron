04dbf438925b3ec0520edadfe257440b6b92a0eb
goolux <30436343+goolux@users.noreply.github.com>
2022-10-18 16:52:42 +0300

    update save_best_model without first_run

--- plot_module/auc_as_function_of_layers.py
+++ plot_module/auc_as_function_of_layers.py
@@ -108,9 +108,10 @@ original_auc_plotting_err = [np.std(i) for i in new_auc_data_original]
 original_auc_plotting = [np.mean(i) for i in new_auc_data_original]
 
 reduction_auc_plotting_err = np.std(np.array(new_auc_data_reduction),axis=1)
-reduction_auc_plotting = np.max(np.array(new_auc_data_reduction),axis=1)
+reduction_auc_plotting = np.mean(np.array(new_auc_data_reduction),axis=1)
 
 plt.errorbar(layers_original, original_auc_plotting, yerr=original_auc_plotting_err,label='original',alpha=0.7)
+# plt.errorbar(layers_original, original_auc_plotting, yerr=original_auc_plotting_err,label='original',alpha=0.7)
 plt.errorbar(layers_reduction, reduction_auc_plotting,yerr=reduction_auc_plotting_err*10,label='reduction',alpha=0.7)
 print(len(original_auc_plotting),batch_counter_original_mean.shape)
 for i in range(len(layers_original)):
