5390b91e224e00d92d6fa01cc847d7a3a2372f89
goolux <30436343+goolux@users.noreply.github.com>
2022-10-18 12:17:05 +0300

    update save_best_model without first_run

--- plot_module/evaluate_sample_entropy.py
+++ plot_module/evaluate_sample_entropy.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 #%%
+if not os.path.exists('sample_entropy_plots'):
+    os.mkdir('sample_entropy_plots')
+tag = "train"
+
+
 def save_large_plot(fig,name):
     mng = plt.get_current_fig_manager()
     mng.full_screen_toggle()
@@ -11,7 +16,7 @@ def save_large_plot(fig,name):
         name = f"{name[:name.find('.')]}_{tag}_{name[name.find('.'):]}"
     else:
         name =f"{name}_{tag}"
-    fig.savefig(name)
+    fig.savefig(os.path.join('sample_entropy_plots',name))
 dim_size=200
 #%%
 reduction_data=dict()
@@ -21,7 +26,7 @@ original_ci=dict()
 file_list=[]
 key_list=set()
 for i in os.listdir(os.path.join('sample_entropy')):
-    if not str(dim_size)+'d.p' in i:
+    if not str(dim_size)+'d.p' in i or not tag in i:
         continue
     s = i.replace('original_','')
     s = s.replace('sample_entropy_','')
@@ -42,23 +47,35 @@ file_list = sorted(file_list,key=lambda x: int(x[len('sample_entropy_')+(len('re
 
 print(len(key_list))
 #%% validation about files that had been done
-plt.scatter(list(key_list),[1]*len(key_list))
+fig,ax=plt.subplots()
+
+ax.scatter(list(key_list),[1]*len(key_list))
+save_large_plot(fig,'files_that_had_been_done.png')
 plt.show()
 #%%
+fig,ax=plt.subplots()
+
 avarage_diff=[]
 for k in key_list:
     avarage_diff.append(original_data[k]-reduction_data[k])
 avarage_diff = np.array(avarage_diff)
-plt.errorbar(np.arange(avarage_diff.shape[1]),np.mean(avarage_diff,axis=0),yerr=np.std(avarage_diff,axis=0))
-
+ax.errorbar(np.arange(avarage_diff.shape[1]),np.mean(avarage_diff,axis=0),yerr=np.std(avarage_diff,axis=0))
+save_large_plot(fig,'error_between_the_same_input.png')
+plt.show()
 #%%
+fig,ax=plt.subplots()
+
 indexes=[1]
 indexes = np.array(list(key_list))[indexes]
 for k in indexes:
-    p = plt.plot(original_data[k])
+    p = ax.plot(original_data[k])
     color = p[0].get_color()
-    plt.plot(reduction_data[k],'--',color=color)
+    ax.plot(reduction_data[k],'--',color=color)
+save_large_plot(fig,'different_between_the_same_input.png')
+plt.show()
 #%%
+fig,ax=plt.subplots()
+
 avarage_original=[]
 avarage_reduction=[]
 for k in key_list:
@@ -66,12 +83,15 @@ for k in key_list:
     avarage_reduction.append(reduction_data[k])
 avarage_original=np.array(avarage_original)
 avarage_reduction=np.array(avarage_reduction)
-plt.plot(np.mean(avarage_original,axis=0),label='original')
-plt.plot(np.mean(avarage_reduction,axis=0),label='reduction')
-plt.legend()
+ax.plot(np.mean(avarage_original,axis=0),label='original')
+ax.plot(np.mean(avarage_reduction,axis=0),label='reduction')
+ax.legend()
+save_large_plot(fig,'avarage_trend.png')
 plt.show()
 
 #%%
+fig,ax=plt.subplots()
+
 avarage_original=[]
 avarage_reduction=[]
 for k in key_list:
@@ -79,21 +99,24 @@ for k in key_list:
     avarage_reduction.append(reduction_data[k])
 avarage_original=np.array(avarage_original)
 avarage_reduction=np.array(avarage_reduction)
-plt.errorbar(np.arange(avarage_original.shape[1]),np.mean(avarage_original,axis=0),yerr=np.std(avarage_original,axis=0),label='original')
-plt.errorbar(np.arange(avarage_reduction.shape[1]),np.mean(avarage_reduction,axis=0),yerr=np.std(avarage_reduction,axis=0),label='reduction')
-plt.legend()
+ax.errorbar(np.arange(avarage_original.shape[1]),np.mean(avarage_original,axis=0),yerr=np.std(avarage_original,axis=0),label='original')
+ax.errorbar(np.arange(avarage_reduction.shape[1]),np.mean(avarage_reduction,axis=0),yerr=np.std(avarage_reduction,axis=0),label='reduction')
+ax.legend()
+save_large_plot(fig,'avarage_trend_with_error.png')
 plt.show()
 
 #%%
+
 r_ci_arr=[]
 o_ci_arr=[]
 import matplotlib.patches as mpatches
+fig,ax=plt.subplots()
 
 for i,k in enumerate(key_list):
     # plt.scatter(i,)
     r_ci_arr.append(reduction_ci[k])
     o_ci_arr.append(original_ci[k])
-parts = plt.violinplot(r_ci_arr, showmeans=True,showextrema = True, showmedians = True)
+parts = ax.violinplot(r_ci_arr, showmeans=True,showextrema = True, showmedians = True)
 for pc in ('cbars','cmins','cmaxes','cmeans','cmedians'):
     pc=parts[pc]
     print(dir(pc))
@@ -103,7 +126,7 @@ for pc in ('cbars','cmins','cmaxes','cmeans','cmedians'):
 
     pc.set_alpha(0.5)
 # pc1=pc['bodies'][0].get_facecolor().flatten()
-parts = plt.violinplot(o_ci_arr, showmeans=True,showextrema = True, showmedians = True)
+parts = ax.violinplot(o_ci_arr, showmeans=True,showextrema = True, showmedians = True)
 for pc in ('cbars','cmins','cmaxes','cmeans','cmedians'):
     pc=parts[pc]
     print(dir(pc))
@@ -113,7 +136,8 @@ for pc in ('cbars','cmins','cmaxes','cmeans','cmedians'):
     pc.set_alpha(0.5)
 # pc2=pc['bodies'][0].get_facecolor().flatten()
 
-plt.legend([mpatches.Patch(color='blue'),mpatches.Patch(color='red')], ['reduction','original'])
+ax.legend([mpatches.Patch(color='blue'),mpatches.Patch(color='red')], ['reduction','original'])
+save_large_plot(fig,'violinplot_overlap.png')
 # plt.scatter(np.zeros([len(r_ci_arr)]),r_ci_arr,color='red')
 # plt.scatter(np.ones([len(r_ci_arr)]),o_ci_arr,color='blue')
 plt.show()
