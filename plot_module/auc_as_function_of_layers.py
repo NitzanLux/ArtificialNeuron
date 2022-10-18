5d0de145a7f15dbf3d35cfe6e1d565ae46e982b8
goolux <30436343+goolux@users.noreply.github.com>
2022-10-18 16:31:43 +0300

    update save_best_model without first_run

--- plot_module/auc_as_function_of_layers.py
+++ plot_module/auc_as_function_of_layers.py
@@ -115,9 +115,9 @@ plt.errorbar(layers_reduction, reduction_auc_plotting,uplims=True, lolims=True, 
 for i in range(len(layers_original)):
     print(human_format(batch_counter_original_std[i]))
     print(human_format(batch_counter_original_mean[i]))
-    plt.annotate(human_format(batch_counter_original_mean[i])+r"$\pm$"+human_format(batch_counter_original_std[i]),layers_original[i],original_auc_plotting[i])
+    plt.annotate(human_format(batch_counter_original_mean[i])+r" $\pm$ "+human_format(batch_counter_original_std[i]),layers_original[i],original_auc_plotting[i])
 for i in range(len(layers_reduction)):
-    plt.annotate(human_format(batch_counter_reduction_mean[i])+r"$\pm$"+human_format(batch_counter_reduction_std[i])',layers_reduction[i],reduction_auc_plotting[i])
+    plt.annotate(human_format(batch_counter_reduction_mean[i])+r" $\pm$ "+human_format(batch_counter_reduction_std[i]),layers_reduction[i],reduction_auc_plotting[i])
 plt.legend()
 plt.show()
 plt.savefig('comparison.png')
