dd2790c00b04060c81a8db13cb5f7682ba5d6923
goolux <30436343+goolux@users.noreply.github.com>
2022-10-18 19:14:12 +0300

    update save_best_model without first_run

--- plot_module/auc_as_function_of_layers.py
+++ plot_module/auc_as_function_of_layers.py
@@ -129,9 +129,9 @@ plt.errorbar(layers_reduction, reduction_auc_plotting,yerr=reduction_auc_plottin
 print(len(original_auc_plotting),batch_counter_original_mean.shape)
 for i in range(len(layers_original)):
 
-    plt.annotate(str(human_format(batch_counter_original_mean[i])+r" $\pm$ "+human_format(batch_counter_original_std[i])), (layers_original[i],original_auc_plotting[i]-0.05), fontsize=FONT_SIZE,color=(255/255., 57/255., 70/255.))
+    plt.annotate(str(human_format(batch_counter_original_mean[i])+r" $\pm$ "+human_format(batch_counter_original_std[i])), (layers_original[i],original_auc_plotting[i]-0.005), fontsize=FONT_SIZE,color=(255/255., 57/255., 70/255.))
 for i in range(len(layers_reduction)):
-    plt.annotate(human_format(batch_counter_reduction_mean[i]) +r" $\pm$ " + human_format(batch_counter_reduction_std[i]), (layers_reduction[i],reduction_auc_plotting[i]-0.05), fontsize=FONT_SIZE,color=(0/255., 90/255., 216/255.))
+    plt.annotate(human_format(batch_counter_reduction_mean[i]) +r" $\pm$ " + human_format(batch_counter_reduction_std[i]), (layers_reduction[i],reduction_auc_plotting[i]-0.005), fontsize=FONT_SIZE,color=(0/255., 90/255., 216/255.))
 plt.legend()
 plt.show()
 plt.savefig('comparison__.png')
