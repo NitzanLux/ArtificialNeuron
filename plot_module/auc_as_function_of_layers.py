4b169334fc84cd4bfb7c763c8e84c8518b26765f
goolux <30436343+goolux@users.noreply.github.com>
2022-10-23 12:18:00 +0300

    add v as an option for sample entropy

--- plot_module/auc_as_function_of_layers.py
+++ plot_module/auc_as_function_of_layers.py
@@ -148,7 +148,7 @@ for i ,l in enumerate(range(max(len(layers_original),len(layers_reduction)))):
             out_str+='*'
         if p_value[i]<0.0005:
             out_str+='*'
-        plt.annotate(out_str,(l,max(np.max(new_auc_data_reduction[:,i]),np.max(new_auc_data_original[:,i]))+0.001),color='black')
+        plt.annotate(out_str,(l,max(np.max(new_auc_data_reduction[i,:]),np.max(new_auc_data_original[i,:]))+0.001),color='black')
 plt.legend()
 plt.title('AUC as a function of layers.')
 plt.xlabel('Number of Layers')
