13bebf11b857327466506f80db9ea96e5bda630d
goolux <30436343+goolux@users.noreply.github.com>
2022-10-23 11:21:26 +0300

    add v as an option for sample entropy

--- plot_module/sample_entropy.py
+++ plot_module/sample_entropy.py
@@ -87,7 +87,7 @@ if __name__ == "__main__":
     number_of_clusters=5
     job_factory = SlurmJobFactory("cluster_logs")
 
-
+    assert args.sv in{'s','v'}
     parent_dir_path = args.parent_dir_path
     # size = len(GroundTruthData.load(os.path.join( 'evaluations', 'ground_truth', gt_name + '.gteval')))
     list_dir_parent=os.listdir(parent_dir_path)
@@ -100,6 +100,6 @@ if __name__ == "__main__":
     for i in range(number_of_clusters):
         pathes=list_dir_parent[i*jumps:min((i+1)*jumps,len(list_dir_parent))]
         print(pathes)
-        use_voltage = not args.use_voltage.lower() in {"false", '0', ''}
+        use_voltage = arga.sv=='v'
         job_factory.send_job(f"sample_entropy{args.tag}_{i}_{MAX_INTERVAL}d", f'python -c "from plot_module.sample_entropy import get_sample_entropy; get_sample_entropy('+"'"+args.tag+"'"+f',{pathes},{use_voltage})"',**keys)
         print('job sent')
