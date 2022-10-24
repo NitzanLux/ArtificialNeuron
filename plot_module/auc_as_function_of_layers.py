68542f47a73a056bf5c36db2295a16f9f6b2eb92
goolux <30436343+goolux@users.noreply.github.com>
2022-10-24 15:10:13 +0300

    add v as an option for sample entropy

--- plot_module/auc_as_function_of_layers.py
+++ plot_module/auc_as_function_of_layers.py
@@ -189,7 +189,7 @@ props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
 text = '\n'.join(text)
 # place a text box in upper left in axes coords
 ann = plt.annotate(text,
-                  xy=(0.05, 0.95),xycoords='axes fraction',textcoords='axes fraction',
+                  xy=(0.01, 0.99),xycoords='axes fraction',textcoords='axes fraction',
                   bbox=dict(boxstyle="square", fc="w"),ha='left', va='top')
 plt.legend(loc=4,)
 plt.title('AUC as a function of layers.')
