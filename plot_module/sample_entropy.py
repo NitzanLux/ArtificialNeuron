bcf2ca129e9563e105d45d2c69ec5f8a3eca5825
goolux <30436343+goolux@users.noreply.github.com>
2022-10-30 12:22:05 +0200

    add v as an option for sample entropy

--- plot_module/sample_entropy.py
+++ plot_module/sample_entropy.py
@@ -117,5 +117,5 @@ if __name__ == "__main__":
         print(len(pathes))
         use_voltage = args.sv=='v'
         print(range(i*jumps,min((i+1)*jumps,len(list_dir_parent))))
-        job_factory.send_job(f"sample_entropy{args.tag}_{i}_{MAX_INTERVAL}d", f'python -c "from plot_module.sample_entropy import get_sample_entropy; get_sample_entropy('+"'"+args.tag+"'"+f',{pathes},{use_voltage},{i*jumps})"',**keys)
+        job_factory.send_job(f"sample_entropy{args.tag}_{i}_{MAX_INTERVAL}d", f'python -c "from plot_module.sample_entropy import get_sample_entropy; get_sample_entropy('+"'"+args.tag+"'"+f',{pathes},{use_voltage},{i*jumps},{use_derivative})"',**keys)
         print('job sent')
