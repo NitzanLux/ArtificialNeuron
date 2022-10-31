8bf6d13072e8b8d263e60ea0f63b957584ee36f3
goolux <30436343+goolux@users.noreply.github.com>
2022-10-31 19:15:37 +0200

    add v as an option for sample entropy

--- plot_module/sample_entropy.py
+++ plot_module/sample_entropy.py
@@ -1,6 +1,6 @@
 from model_evaluation_multiple import GroundTruthData
 import matplotlib.pyplot as plt
-import EntropyHub as EH
+import EntropyHub as EH,SampEn
 import os
 import numpy as np
 import time
