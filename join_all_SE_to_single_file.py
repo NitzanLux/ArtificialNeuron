321d340e4a15adbf98725190022fc78cd29077ac
goolux <30436343+goolux@users.noreply.github.com>
2022-11-04 10:12:28 +0200

    add v as an option for sample entropy

--- join_all_SE_to_single_file.py
+++ join_all_SE_to_single_file.py
@@ -2,6 +2,10 @@ import os
 import pickle
 
 for i in os.listdir('sample_entropy'):
+    cur_path =  os.path.join('sample_entropy',i)
+    if not os.path.isdir(cur_path):
+        os.remove(cur_path)
+for i in os.listdir('sample_entropy'):
     cut_path =os.path.join('sample_entropy',i)
     if os.path.isdir(cut_path):
         data_dict={}
@@ -11,4 +15,3 @@ for i in os.listdir('sample_entropy'):
                 data_dict[i]=pickle.load(f)
         with open(f'{i}.pkl','wb') as f_o:
             pickle.dump(data_dict, f_o)
-
