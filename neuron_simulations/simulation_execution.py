c143a396033297116e34570a19af704bd01e4a3f
goolux <30436343+goolux@users.noreply.github.com>
2022-11-06 19:33:33 +0200

    add v as an option for sample entropy

--- neuron_simulations/simulation_execution.py
+++ neuron_simulations/simulation_execution.py
@@ -64,12 +64,12 @@ for i, f in enumerate(only_files):
         params_string = 'python3 -m neuron_simulations.simulate_L5PC_ergodic %s -i $SLURM_JOB_ID' % (
                     "-f '" + str(os.path.join(directory,
                                               f)) + "' -d '" + base_directory + "_" + directory_name +  ('NMDA' if NMDA_or_AMPA else 'AMPA') +
-                    "' -na %s "% ('N' if NMDA_or_AMPA else 'A')+' -gmax_ampa '+str(args.gmaxampa))
+                    "' -na %s "% ('N' if NMDA_or_AMPA else 'A')+' -gmax_ampa '+str(args.gmax_ampa))
     else:
         params_string = params_string + '&& python3 -m neuron_simulations.simulate_L5PC_ergodic %s -i -1' % (
                     "-f '" + str(os.path.join(directory,
                                               f)) + "' -d '" + base_directory + "_" + directory_name +  ('NMDA' if NMDA_or_AMPA else 'AMPA') +
-                    "' -na %s"% ('N' if NMDA_or_AMPA else 'A')+' -gmax_ampa '+str(args.gmaxampa))
+                    "' -na %s"% ('N' if NMDA_or_AMPA else 'A')+' -gmax_ampa '+str(args.gmax_ampa))
 
     if i%files_per_cpu==files_per_cpu-1 or i==len(only_files)-1:
         job_factory.send_job("%s_%s"%( ('NMDA' if NMDA_or_AMPA else 'AMPA')+"_simulation",base_directory[:15]+"_"+directory_name), params_string,filename_index=i//files_per_cpu)
