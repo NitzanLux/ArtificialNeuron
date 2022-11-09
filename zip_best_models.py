dd3e0926a6cea506bec3c54d96f6a13b803e1ef0
goolux <30436343+goolux@users.noreply.github.com>
2022-11-09 17:54:06 +0200

    update_problems with model_ev  and zip best msc script

--- zip_best_models.py
+++ zip_best_models.py
@@ -3,5 +3,5 @@ import subprocess
 zip_arr=[]
 for i in os.listdir(os.path.join('models','NMDA')):
     if 'd_r_comparison_ss_7' in i:
-        zip_arr.append(i)
+        zip_arr.append(os.path.join('models','NMDA',i))
 p = subprocess.Popen(["zip",'-r','models_for_msc_proj.zip',*zip_arr])
