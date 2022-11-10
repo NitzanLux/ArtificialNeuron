5a7357f7a8fa7ff7a8b71bc3f55f9edd3b6e467a
goolux <30436343+goolux@users.noreply.github.com>
2022-11-10 16:00:01 +0200

    update_problems with model_ev  and zip best msc script

--- plot_module/auc_as_function_of_layers.py
+++ plot_module/auc_as_function_of_layers.py
@@ -208,7 +208,7 @@ text=[]
 for k in sorted(p_values_dict.keys(),key=lambda x:x):
     key = {'size':len(astriks_dict[k])*150+450}
     out  = astriks_dict[k].replace("*", r"\ast")
-    plt.scatter([],[],marker=f'${out}$',color='black',s=,label='$p_{value}$<5e-%d'%k,**key)
+    plt.scatter([],[],marker=f'${out}$',color='black',label='$p_{value}$<5e-%d'%k,**key)
     # text.append(astriks_dict[k]+' - $p_{value}$<5e-%d'%k)
     # print(' - $p_{value}$<5e-%d'%k,k)
 props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
