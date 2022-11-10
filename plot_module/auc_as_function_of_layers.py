3e446fd9d406dc99a4d7772e32b84343aee70b83
goolux <30436343+goolux@users.noreply.github.com>
2022-11-10 10:38:24 +0200

    update_problems with model_ev  and zip best msc script

--- plot_module/auc_as_function_of_layers.py
+++ plot_module/auc_as_function_of_layers.py
@@ -212,6 +212,7 @@ props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
 text = '\n'.join(text)
 leg = plt.legend(loc=4,)
 # place a text box in upper left in axes coords
+plt.draw()
 p = leg.get_window_extent()
 ann = plt.annotate(text,
                   (p.p0[0], p.p1[1]), (p.p0[0], p.p1[1])#,xycoords='axes fraction',textcoords='axes fraction'
