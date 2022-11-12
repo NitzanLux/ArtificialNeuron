ef46b6ebcfd98394c79020664c9e92611406bb21
goolux <30436343+goolux@users.noreply.github.com>
2022-11-12 18:39:23 +0200

    update_problems with model_ev  and zip best msc script

--- plot_module/comparison_plots.py
+++ plot_module/comparison_plots.py
@@ -159,6 +159,8 @@ alpha = 0.5
 # c = matplotlib.cm.get_cmap('jet', max_layer)
 # color_function = lambda l: c(l / max_layer)
 color_function = lambda l: 'orange'
+gtc=(0, 101, 161)
+moc=(236, 82, 59)
 # margins
 right_margin = 0.1
 left_margin = 0.05
@@ -208,10 +210,10 @@ save_large_plot(fig, 'evaluation_plots/raster_pipline.png')
 fig, ax = plt.subplots()
 # ax.get_xaxis().set_ticks([])
 
-ax.plot(output_x_range, reduction_output_v, color='blue', label='compartmental reduction model',alpha=alpha)
+ax.plot(output_x_range, reduction_output_v, color=gtc, label='compartmental reduction model',alpha=alpha)
 for v, s, l, th in model_evaluation_reduction:
     print(l)
-    ax.plot(output_x_range, v, color=color_function(l), label=f"{l} layers", alpha=alpha)
+    ax.plot(output_x_range, v, color=moc, label=f"{l} layers", alpha=alpha)
 # ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 mng = plt.get_current_fig_manager()
 mng.full_screen_toggle()
@@ -222,11 +224,11 @@ plt.show()
 # fig.axes[2].set_position([right_margin_position,ax1_pos.y0-ax2_pos.height-twin_graph_margin,ax2_pos.width,ax2_pos.height])
 # ax2_pos = fig.axes[2].get_position()
 fig, ax = plt.subplots()
-ax.plot(output_x_range, reduction_output_s, color='blue', label='compartmental reduction model',alpha=alpha)
+ax.plot(output_x_range, reduction_output_s, color=gtc, label='compartmental reduction model',alpha=alpha)
 for v, s, l, th in model_evaluation_reduction:
     print(l,2)
 
-    ax.plot(output_x_range, s, color=color_function(l), label=f"{l} layers", alpha=alpha)
+    ax.plot(output_x_range, s, color=moc, label=f"{l} layers", alpha=alpha)
 # ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 mng = plt.get_current_fig_manager()
 mng.full_screen_toggle()
@@ -237,11 +239,11 @@ plt.show()
 # fig.axes[3].set_position([right_margin_position,ax3_pos.y0,ax3_pos.width,ax3_pos.height])
 # fig.axes[3].get_xaxis().set_ticks([])
 fig, ax = plt.subplots()
-ax.plot(output_x_range, original_output_v, color='blue', label='compartmental model', alpha=alpha)
+ax.plot(output_x_range, original_output_v, color=gtc, label='compartmental model', alpha=alpha)
 for v, s, l, th in model_evaluation_original:
     print(th, l)
     v[s >= th] = 20
-    ax.plot(output_x_range, v, color=color_function(l), label=f"{l} layers", alpha=alpha)
+    ax.plot(output_x_range, v, color=moc, label=f"{l} layers", alpha=alpha)
 # ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 save_large_plot(fig, 'evaluation_plots/pipeline_original_v.png')
 plt.show()
@@ -250,12 +252,12 @@ plt.show()
 # fig.axes[4].set_position([right_margin_position,ax3_pos.y0-ax4_pos.height-twin_graph_margin,ax4_pos.width,ax4_pos.height])
 # ax4_pos = fig.axes[4].get_position()
 fig, ax = plt.subplots()
-ax.plot(output_x_range, original_output_s, color='blue', label='compartmental model',alpha=alpha)
+ax.plot(output_x_range, original_output_s, color=gtc, label='compartmental model',alpha=alpha)
 for v, s, l, th in model_evaluation_original:
     v[s >= th] = 20
     print(l,3)
 
-    ax.plot(output_x_range, s, color=color_function(l), label=f"{l} layers", alpha=alpha)
+    ax.plot(output_x_range, s, color=moc, label=f"{l} layers", alpha=alpha)
 # ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 mng = plt.get_current_fig_manager()
 mng.full_screen_toggle()
