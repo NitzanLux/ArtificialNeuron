aa3b47e15a1bc11c373a746e22cb75fb13c8237b
goolux <30436343+goolux@users.noreply.github.com>
2023-07-27 13:55:53 +0300

    debugging seg fault

--- train_nets/neuron_network/recursive_neuronal_model.py
+++ train_nets/neuron_network/recursive_neuronal_model.py
@@ -229,10 +229,10 @@ class RecursiveNeuronModel(nn.Module):
             neuronal_model_data = pickle.load(outp)
         print("start loading model...")
         get_standard_model = importlib.import_module(
-            f"neuron_simulations.neuron_models.{config['biophysical_model']}.get_standard_model")
-        print(dir(get_standard_model))
-        # bio_mod = get_standard_model.create_cell()
-        # print(bio_mod)
+            f"neuron_simulations.neuron_models.{config['biophysical_model']}.get_standard_model.create_cell")
+        print(get_standard_model)
+        bio_mod = get_standard_model()
+        print(bio_mod)
         exit(0)
         if "biophysical_model" not in config or config['biophysical_model']=='L5PC_david':
             bio_mod = get_L5PC()
